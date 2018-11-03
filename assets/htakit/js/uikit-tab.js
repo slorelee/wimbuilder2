01bdc89872d922a5a49d5e38cd6fd019d336e137
slore <slorelee@gmail.com>
2018-11-03 10:52:15 +0800

    update uitab

--- assets/htakit/js/uikit-tab.js
+++ assets/htakit/js/uikit-tab.js
@@ -4,6 +4,29 @@
       header:['Item1', 'Item2'],
       active:0
     }
+    function regist_click_event(elem, arg) {
+       elem.click(arg, function(e) {
+          var _this = e.data.parent;
+          var ul = e.data.ul;
+          var i = e.data.i;
+          ul.children('.uk-active').removeClass('uk-active');
+          _this.children('.tab-active').removeClass('tab-active');
+
+          $(this).addClass('uk-active');
+          _this.children('.tab' + i).addClass('tab-active');
+
+        });
+    }
+    if (options === true) { /* regist click event only */
+      var _this = this;
+      var ul = this.children('ul');
+      var i = 0;
+      ul.children('li').each(function() {
+        regist_click_event($(this), {parent:_this, ul:ul, i:i});
+        i++;
+      });
+      return $(_this);
+    }
     var options = $.extend(defaults, options);
     this.each(function(){
       var _this = $(this);
@@ -17,17 +40,7 @@
           li.addClass('uk-active');
           _this.children('.tab' + i).addClass('tab-active');
         }
-        li.click({parent:_this, ul:ul, i:i}, function(e) {
-          var _this = e.data.parent;
-          var ul = e.data.ul;
-          var i = e.data.i;
-          ul.children('.uk-active').removeClass('uk-active');
-          _this.children('.tab-active').removeClass('tab-active');
-
-          $(this).addClass('uk-active');
-          _this.children('.tab' + i).addClass('tab-active');
-
-        });
+        regist_click_event(li, {parent:_this, ul:ul, i:i});
         ul.append(li);
       }
     })

8d97c2bde99b5d49a3a11938509286011eadc7b5
slore <slorelee@gmail.com>
2018-11-12 01:33:01 +0800

    add a way to skip SOFTWARE registry copy action

--- lib/macros/RegCopy.cmd
+++ lib/macros/RegCopy.cmd
@@ -3,8 +3,6 @@
 rem MACRO:RegCopy
 rem replace KEY_PATH to Src_XXX, Tmp_XXX and call reg.exe COPY
 
-echo [MACRO]RegCopy %*
-
 set "param_key=%~1"
 set "src_key=%param_key:HKLM\Software\=HKLM\Src_Software\%"
 set "tmp_key=%param_key:HKLM\Software\=HKLM\Tmp_Software\%"
@@ -12,4 +10,10 @@ set "tmp_key=%param_key:HKLM\Software\=HKLM\Tmp_Software\%"
 set "src_key=%src_key:HKLM\SYSTEM\=HKLM\Src_SYSTEM\%"
 set "tmp_key=%tmp_key:HKLM\SYSTEM\=HKLM\Tmp_SYSTEM\%"
 
+set "chk_key=%src_key:HKLM\Src_Software\=%"
+if "x%REGCOPY_SKIP_SOFTWARE%"=="x1" (
+  if not "%chk_key%"=="%src_key%" goto :EOF
+)
+
+echo [MACRO]RegCopy %*
 reg copy "%src_key%" "%tmp_key%" /s /f

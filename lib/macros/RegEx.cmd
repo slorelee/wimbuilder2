d04f9e2dd2474d1fcf23e24151c94a55b1bcdd07
slore <slorelee@gmail.com>
2018-11-16 21:22:00 +0800

    update RegEx macro to update registry when key is existing

--- lib/macros/RegEx.cmd
+++ lib/macros/RegEx.cmd
@@ -1,7 +1,13 @@
 set "args=%*"
+if "x%args:~0,8%"=="xHAS_KEY " goto :HAS_KEY_REG
 if "x%args:~0,7%"=="xNO_KEY " goto :NO_KEY_REG
 if "x%args:~0,7%"=="xNO_VAL " goto :NO_VAL_REG
 echo [MACRO]RegEx Error:Invaild paramters(%*).
+goto :EOF
+
+:HAS_KEY_REG
+reg query "%~3" 1>nul 2>nul
+if %errorlevel% EQU 0 reg %args:~8%
 goto :EOF
 
 :NO_KEY_REG

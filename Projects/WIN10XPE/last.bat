6984327972cb8c46755c05cf874c7e215141d78a
slore <slorelee@gmail.com>
2018-11-18 10:11:50 +0800

    add PECMD.exe startup entry

--- Projects/WIN10XPE/last.bat
+++ Projects/WIN10XPE/last.bat
@@ -1,2 +1,60 @@
-rem call PERegPorter.bat Tmp UNLOAD 1>nul
-rem xcopy /E /Y "%~dp0SOFTWARE" X:\Windows\System32\Config\
+rem display startmenu folders/shortcuts name with language
+attrib +s "X:\Users\Default\AppData\Roaming\Microsoft\Windows\SendTo"
+attrib +s "X:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessibility"
+attrib +s "X:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories"
+attrib +s "X:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools"
+
+rem startup with pecmd.exe
+:PECMD_ENTRY
+if not exist X:\Windows\System32\pecmd.exe goto :STARTNET_ENTRY
+reg add HKLM\Tmp_System\Setup /v CmdLine /d "Pecmd.exe Main %%Windir%%\System32\Pecmd.ini" /f
+goto :STARTUP_ENTRY_END
+
+:STARTNET_ENTRY
+rem startup explorer shell
+
+call AddFiles %0 :end_files
+goto :end_files
+windows\system32\taskkill.exe
+:end_files
+
+
+rem set X32=X:\Windows\System32
+del /q X:\setup.exe
+del /q X:\Windows\System32\winpeshl.ini
+del /q X:\Windows\System32\WallpaperHost.exe
+
+rem update startnet.cmd
+call OpenTextFile JS X:\Windows\System32\startnet.cmd %0 :end_startnet_edit
+goto :end_startnet_edit
+
+TXT.before('wpeinit').insert('set USERPROFILE=X:\\Users\\Default');
+TXT.append('\r\n\
+taskkill /f /im WallpaperHost.exe\r\n\
+start explorer.exe\r\n\
+');
+
+:end_startnet_edit
+
+echo ping -n 2 127.0.0.1 1^>nul>>X:\Windows\System32\startnet.cmd
+echo taskkill /f /im explorer.exe>>X:\Windows\System32\startnet.cmd
+echo explorer.exe>>X:\Windows\System32\startnet.cmd
+echo cmd.exe>>X:\Windows\System32\startnet.cmd
+
+:STARTUP_ENTRY_END
+
+rem use prepared HIVE files
+call PERegPorter.bat Tmp UNLOAD 1>nul
+
+call :FULLREG DEFAULT
+call :FULLREG SOFTWARE
+call :FULLREG SYSTEM
+call :FULLREG COMPONENTS
+call :FULLREG DRIVERS
+goto :EOF
+
+:FULLREG
+if exist "%~dp0%1" (
+   xcopy /E /Y "%~dp0%1" X:\Windows\System32\Config\
+)
+

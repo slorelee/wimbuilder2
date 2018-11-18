3fdf751a51ac038296c7fd84c0567aed9c080687
slore <slorelee@gmail.com>
2018-11-18 11:15:23 +0800

    define Battery icon required files

--- Projects/WIN10XPE/01-Components/00-Shell/main.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/main.bat
@@ -1,2 +1,15 @@
+rem Battery icon should be optional, skip now
+goto :end_files
+call AddFiles %0 :end_files
+goto :end_files
+; Battery icon - In Winre.wim inf: hidbatt.inf,cmbatt.inf - drivers: battc.sys,HidBatt.sys,CmBatt.sys - system32: umpo.dll,umpnpmgr.dll
+
+\Windows\INF\battery.inf
+\Windows\INF\c_battery.inf
+\Windows\System32\batmeter.dll
+\Windows\System32\??-??\batmeter.dll.mui
+
+:end_files
+
 call Explorer\submain.bat
 call WinXShell\submain.bat

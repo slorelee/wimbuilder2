9921d976030caf471ba238281e95e791263e2716
slore <slorelee@gmail.com>
2018-11-19 20:09:07 +0900

    fix issue that the SwitchDarkTheme.cmd can't work

--- Projects/WIN10XPE/00-Configures/System/last.bat
+++ Projects/WIN10XPE/00-Configures/System/last.bat
@@ -3,6 +3,8 @@ rem Explorer Dark or Light Theme
 if "x%opt[system.darktheme]%"=="xtrue" (
     rem check WB_PE_VER > 17700
     reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+) else (
+    reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f
 )
 
 rem NumLock on/off

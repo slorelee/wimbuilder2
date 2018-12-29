8665ad365ccb91b673d0b7c2c3de650fdc11d1a6
slore <slorelee@gmail.com>
2018-12-29 14:27:45 +0900

    added option for doing AddFiles in batch(merge) mode

--- lib/macros/AddFiles.cmd
+++ lib/macros/AddFiles.cmd
@@ -21,4 +21,7 @@ if "x%_WB_TMP_DIR%"=="x" goto :EOF
 
 rem CALL AddFiles.vbs
 cscript //nologo  "%~dp0\AddFiles.vbs" %* "%_WB_TMP_DIR%\_AddFiles.txt"
+
+if "x%AddFiles_Mode%"=="xbatch" goto :EOF
+if "x%AddFiles_Mode%"=="xmerge" goto :EOF
 call DoAddFiles

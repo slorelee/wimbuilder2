4a59206789e5d5ddb8b580d9106dcb17b9322a84
slore <slorelee@gmail.com>
2018-12-29 22:15:10 +0800

    add Customization patch

--- Projects/WIN10XPE/00-Configures/System/last.bat
+++ Projects/WIN10XPE/00-Configures/System/last.bat
@@ -1,11 +1,12 @@
 
-rem Explorer Dark or Light Theme
-if "x%opt[system.darktheme]%"=="xtrue" (
-    rem check WB_PE_VER > 17700
-    reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f
-) else (
-    reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f
+rem Fbwf Cache Size. Limited to 1024 Mb with x86
+set _fbwf_size=%opt[config.fbwf.cache]%
+if "x%_fbwf_size%"=="x" set _fbwf_size=1024
+if %_fbwf_size% GTR 1024 (
+  if "%WB_PE_ARCH%"=="x86" set _fbwf_size=1024
 )
+reg add HKLM\Tmp_System\ControlSet001\Services\FBWF /v WinPECacheThreshold /t REG_DWORD /d %_fbwf_size% /f
+set _fbwf_size=
 
 rem NumLock on/off
 if not "x%opt[system.numlock]%"=="xfalse" (
---
+++ Projects/WIN10XPE/00-Configures/z-Customization/last.bat
@@ -0,0 +1,30 @@
+
+rem Explorer Dark or Light Theme
+if "x%opt[system.darktheme]%"=="xtrue" (
+    rem check WB_PE_VER > 17700
+    reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+) else (
+    reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 1 /f
+)
+
+if not exist "%X_SYS%\WindowsPowerShell\v1.0\powershell.exe" (
+    rem use cmd.exe on directorybackground than powershell.exe
+    reg delete HKLM\Tmp_Software\Classes\Directory\background\shell\Powershell /v ShowBasedOnVelocityId /f
+    reg add HKLM\Tmp_Software\Classes\Directory\background\shell\cmd /v HideBasedOnVelocityId /t REG_DWORD /d 0x639bc8 /f
+    reg delete HKLM\Tmp_Software\Classes\Directory\background\shell\cmd /v HideBasedOnVelocityId /f
+    reg add HKLM\Tmp_Software\Classes\Directory\background\shell\cmd /v ShowBasedOnVelocityId /t REG_DWORD /d 0x639bc8 /f
+    rem always show the menu item
+    reg delete HKLM\Tmp_Software\Classes\Directory\background\shell\cmd /v Extended /f
+)
+
+rem // Shortcuts with 'shortcut' name and transparent icon
+if "x%opt[tweak.shortcut.noarrow]%"=="xtrue" (
+    call X2X
+    reg add "HKLM\Tmp_Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" /v 29 /t REG_EXPAND_SZ /d "%%SystemRoot%%\system32\transparent.ico" /f
+)
+
+if "x%opt[tweak.shortcut.nonosuffix]%"=="xtrue" (
+    reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Explorer /v link /t REG_BINARY /d 15000000 /f
+)
+
+

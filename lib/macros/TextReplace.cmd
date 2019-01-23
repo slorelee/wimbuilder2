047fb8c6e39d44c2a0f0fba515dececd30e616a6
slore <slorelee@gmail.com>
2019-01-23 21:28:58 +0800

    update TextReplace macro

--- lib/macros/TextReplace.cmd
+++ lib/macros/TextReplace.cmd
@@ -1,4 +1,7 @@
 @echo off
 
+rem NOTICE: can't use // at begin, so use #// to instead
+rem ESCAPE: #q => ", #s => #, #p => %, #t => \t, #r => \r, #n => \n
+
 echo [MACRO]TextReplace %*
 cscript //nologo "%~dp0\TextReplace.js" %*

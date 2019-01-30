35ef7adacac6ec763b0a0ebcc55364f78274b7f1
nonamemanSEO <46984966+nonamemanSEO@users.noreply.github.com>
2019-01-30 12:51:44 +0900

    Update ko-KR.js
    add more translation to update new funtion

--- Projects/WIN10XPE/00-Configures/System/ko-KR.js
+++ Projects/WIN10XPE/00-Configures/System/ko-KR.js
@@ -6,6 +6,8 @@ patch_i18n = {
     "Administrator Options":"Administrator 옵션",
     "Enable Administrator account":"Administrator 계정 활성화",
     "Switch to Administrator for(sec):":"Administrator 계정으로 전환 할 때까지의 대기 시간 (초):",
+    "Background":"배경화면",
+    "use wallpaper image":"배경 이미지 사용",
     "If enabled, will use the full SOFTWARE hive":"활성화되면 full SOFTWARE 하이브가 자동 선택됨",
     "The switching will be black screen for seconds without DWM":"DWM 서비스를 사용할 수 없을때에는 전환과정에 몇초간 검은 화면이 표시됩니다",
     "Theme Options":"테마 옵션",

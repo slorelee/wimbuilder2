9e8f5faea40ab88bbabac1a70649d1b3b12e3be8
slore <slorelee@gmail.com>
2019-02-09 22:45:50 +0800

    deal with shell.taskbar.combine option

--- Projects/WIN10XPE/01-Components/00-Shell/last.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/last.bat
@@ -16,3 +16,12 @@ rem reg add "HKLM\Tmp_Default\Control Panel\Desktop" /v Wallpaper /d X:\Windows\
 rem reg add "HKLM\Tmp_Default\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperSource /d X:\Windows\Web\Wallpaper\Windows\img0.jpg /f
 reg add "HKLM\Tmp_Software\Microsoft\Windows NT\CurrentVersion\WinPE" /v CustomBackground /t REG_EXPAND_SZ /d X:\Windows\Web\Wallpaper\Windows\img0.jpg /f
 
+rem // 0=Always combine, hide labels, 1=Combine when taskbar is full,2=Never combine
+set TaskbarCombineType=2
+if "x%opt[shell.taskbar.combine]%"=="xalways" (
+    set TaskbarCombineType=0
+) else if "x%opt[shell.taskbar.combine]%"=="xauto" (
+    set TaskbarCombineType=1
+)
+reg add HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v TaskbarGlomLevel /t REG_DWORD /d %TaskbarCombineType% /f
+set TaskbarCombineType=

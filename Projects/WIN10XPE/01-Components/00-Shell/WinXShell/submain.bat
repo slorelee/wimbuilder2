44ecc8c3d6aa7049a6ae3b156c9645f337957094
slore <slorelee@gmail.com>
2019-03-19 00:04:19 +0800

    update WinXShell

--- Projects/WIN10XPE/01-Components/00-Shell/WinXShell/submain.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/WinXShell/submain.bat
@@ -1,5 +1,10 @@
-if not "x%opt[shell.app]%"=="xwinxshell" goto :EOF
 cd /d "%~dp0"
+if not exist X_%WB_PE_ARCH% goto :EOF
 call X2X
+
+rem Grant right for Administrator
+call _ACLRegKey Tmp_Software\Classes\ms-settings
+
+if not "x%opt[shell.app]%"=="xwinxshell" goto :EOF
 call SharedPatch NewBrowseDlg
 

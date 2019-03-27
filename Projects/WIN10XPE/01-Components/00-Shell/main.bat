19429000cb927c08edc889b1e272fb9f0ce7d633
slore <slorelee@gmail.com>
2019-03-27 20:49:37 +0800

    add resources for desktop background contextmenu

--- Projects/WIN10XPE/01-Components/00-Shell/main.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/main.bat
@@ -1,13 +1,23 @@
 rem Battery icon should be optional, skip now
-goto :end_files
+
 call AddFiles %0 :end_files
 goto :end_files
+
+; Battery icon should be optional, skip now
++ver < 0
 ; Battery icon - In Winre.wim inf: hidbatt.inf,cmbatt.inf - drivers: battc.sys,HidBatt.sys,CmBatt.sys - system32: umpo.dll,umpnpmgr.dll
 
 \Windows\INF\battery.inf
 \Windows\INF\c_battery.inf
 @\Windows\System32\
 batmeter.dll
++ver*
+
+@\Windows\System32\
+
+; resources for desktop background contextmenu
+Display.dll
+themecpl.dll
 
 :end_files
 

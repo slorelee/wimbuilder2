f06248ebef3e5e1847607606f8a5d3bfdd29fbf8
slore <slorelee@gmail.com>
2019-03-31 12:57:52 +0800

    fix typo(correct Wlansvc services registy path)

--- Projects/WIN10XPE/01-Components/02-Network/last.bat
+++ Projects/WIN10XPE/01-Components/02-Network/last.bat
@@ -2,9 +2,11 @@ rem remove WcmSvc from Wlansvc service's DependOnService
 rem data sample:nativewifip\0RpcSs\0Ndisuio\0Eaphost
 
 set WLANSVC_DEPEND_SERV=
-for /f "tokens=3" %%i in ('reg query HKLM\SOFTWARE\Tmp_SYSTEM\ControlSet001\services\Wlansvc /v DependOnService') do (
+for /f "tokens=3" %%i in ('reg query HKLM\Tmp_SYSTEM\ControlSet001\services\Wlansvc /v DependOnService') do (
   set WLANSVC_DEPEND_SERV=%%i
 )
+if "x%WLANSVC_DEPEND_SERV%"=="x" goto :EOF
+
 set WLANSVC_NEW_DEPEND=%WLANSVC_DEPEND_SERV%
 if not "x%WLANSVC_NEW_DEPEND%"=="x" (
   set WLANSVC_NEW_DEPEND=%WLANSVC_NEW_DEPEND:WcmSvc\0=%
@@ -13,6 +15,6 @@ if not "x%WLANSVC_NEW_DEPEND%"=="x" (
   set WLANSVC_NEW_DEPEND=%WLANSVC_NEW_DEPEND:\0WcmSvc=%
 )
 if not "x%WLANSVC_DEPEND_SERV%"=="x%WLANSVC_NEW_DEPEND%" (
-  echo update Wlansvc depend(%WLANSVC_DEPEND_SERV% To %WLANSVC_NEW_DEPEND%)
-  reg add HKLM\SOFTWARE\Tmp_SYSTEM\ControlSet001\services\Wlansvc /v DependOnService /t REG_MULTI_SZ /d "%WLANSVC_NEW_DEPEND%" /f
+  echo update Wlansvc's DependOnService from [%WLANSVC_DEPEND_SERV%] To [%WLANSVC_NEW_DEPEND%]
+  reg add HKLM\Tmp_SYSTEM\ControlSet001\services\Wlansvc /v DependOnService /t REG_MULTI_SZ /d "%WLANSVC_NEW_DEPEND%" /f
 )

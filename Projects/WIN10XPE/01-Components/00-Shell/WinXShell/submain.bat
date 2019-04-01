5fe1b7250d7d0d53a5001eee902962af5de58b64
slore <slorelee@gmail.com>
2019-04-01 23:11:57 +0800

    update WinXShell

--- Projects/WIN10XPE/01-Components/00-Shell/WinXShell/submain.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/WinXShell/submain.bat
@@ -4,6 +4,7 @@ call X2X
 
 rem Grant right for Administrator
 call _ACLRegKey Tmp_Software\Classes\ms-settings
+call _ACLRegKey Tmp_Software\Classes\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}
 
 if not "x%opt[shell.app]%"=="xwinxshell" goto :EOF
 call SharedPatch NewBrowseDlg

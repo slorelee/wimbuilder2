46fbda1b93c266c13dd8ebbb1a5909af3ee4dc97
slore <slorelee@gmail.com>
2019-04-06 21:54:01 +0800

    fix issue that can't get wim info if there are space character(s) in path

--- assets/htakit/js/lib/wsh.js
+++ assets/htakit/js/lib/wsh.js
@@ -13,6 +13,6 @@ function exec_cmd(cmd) {
     var env = wsh.Environment("PROCESS");
     var tmp = env("WB_ROOT") + "_Factory_\\tmp\\_exec.tmp";
     cmd = eformat(cmd);
-    wsh.Run(cmd + " 1>" + tmp, 0, true);
+    wsh.Run(cmd + " 1> \"" + tmp + "\"", 0, true);
     return load_text_file(tmp);
 }

39d6107076706049659f280c6abc7428d3c2b9f2
slore <slorelee@gmail.com>
2019-04-07 18:08:54 +0800

    apply CustomCompmgmt

--- Projects/WIN10XPE/01-Components/00-Shell/WinXShell/submain.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/WinXShell/submain.bat
@@ -8,4 +8,4 @@ call _ACLRegKey Tmp_Software\Classes\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D
 
 if not "x%opt[shell.app]%"=="xwinxshell" goto :EOF
 call SharedPatch NewBrowseDlg
-
+call SharedPatch CustomCompmgmt

75704565de00b655285958fd69581898abcb13e6
slore <slorelee@gmail.com>
2019-04-17 12:02:18 +0900

    add 4GB, 8GB fbwf cache size option

--- Projects/WIN10XPE/00-Configures/System/last.bat
+++ Projects/WIN10XPE/00-Configures/System/last.bat
@@ -10,6 +10,11 @@ if %_fbwf_size% GTR 1024 (
   if "%WB_PE_ARCH%"=="x86" set _fbwf_size=1024
 )
 reg add HKLM\Tmp_System\ControlSet001\Services\FBWF /v WinPECacheThreshold /t REG_DWORD /d %_fbwf_size% /f
+if exist fbwf_%_fbwf_size%.cfg (
+  echo Enable %_fbwf_size% MB cache size with Windows Embedded Standard's fbwf driver
+  copy /y fbwf_%_fbwf_size%.cfg "%X_WIN%\fbwf.cfg"
+  copy /y fbwf.sys "%X_SYS%\drivers\fbwf.sys"
+)
 set _fbwf_size=
 
 rem NumLock on/off

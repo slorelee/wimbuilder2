e752395cb9100b18e50a88fc506cbeb056674be9
slore <slorelee@gmail.com>
2019-04-28 11:27:07 +0800

    add Microsoft Wubi IME

--- Projects/WIN10XPE/01-Components/IME/Ime_zh-CN.bat
+++ Projects/WIN10XPE/01-Components/IME/Ime_zh-CN.bat
@@ -1,3 +1,14 @@
+if "x%opt[IME.ms_pinyin]%"=="x" set opt[IME.ms_pinyin]=true
+
+set _CTF_DEFAULT={81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E}
+set _CTF_PROFILE={FA550B04-5AD7-411F-A5AC-CA038EC515D7}
+
+if not "%opt[IME.ms_pinyin]%"=="true" (
+    if "x%opt[IME.ms_wubi]%"=="xtrue" (
+        set _CTF_DEFAULT={6A498709-E00B-4C45-A018-8F9E4081AE40}
+        set _CTF_PROFILE={82590C13-F4DD-44F4-BA1D-8667246FDF8E}
+    )
+)
 
 call AddFiles %0 :end_files
 goto :end_files
@@ -21,38 +32,39 @@ goto :end_wow64_files
 :end_wow64_files
 
 :UDPATE_REGISTY
-reg add "HKLM\Tmp_Default\Keyboard Layout\Preload" /v 2 /d 00000409 /f
-reg add "HKLM\Tmp_Default\Keyboard Layout\Preload" /v 1 /d 00000804 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Default /d {00000000-0000-0000-0000-000000000000} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Profile /d {00000000-0000-0000-0000-000000000000} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v KeyboardLayout /t REG_DWORD /d 67699721 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Default /d {81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Profile /d {FA550B04-5AD7-411F-A5AC-CA038EC515D7} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v KeyboardLayout /t REG_DWORD /d 134481924 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\HiddenDummyLayouts /v 00000804 /d 00000804 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v CLSID /d {00000000-0000-0000-0000-000000000000} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v KeyboardLayout /t REG_DWORD /d 67699721 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v Profile /d {00000000-0000-0000-0000-000000000000} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v CLSID /d {81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v KeyboardLayout /t REG_DWORD /d 0 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v Profile /d {FA550B04-5AD7-411F-A5AC-CA038EC515D7} /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\Language /v 00000000 /d 00000804 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\TIP\{81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E}\LanguageProfile\0x00000804\{FA550B04-5AD7-411F-A5AC-CA038EC515D7} /v Enable /t REG_DWORD /d 1 /f
-rem ; is zh-TW IMEs, set enable=0 to hide
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\TIP\{B115690A-EA02-48D5-A231-E3578D2FDF80}\LanguageProfile\0x00000404\{B2F9C502-1742-11D4-9790-0080C882687E} /v Enable /t REG_DWORD /d 0 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\TIP\{531fdebf-9b4c-4a43-a2aa-960e8fcdc732}\LanguageProfile\0x00000404\{6024B45F-5C54-11D4-B921-0080C882687E} /v Enable /t REG_DWORD /d 0 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\TIP\{531fdebf-9b4c-4a43-a2aa-960e8fcdc732}\LanguageProfile\0x00000404\{4BDF9F03-C7D3-11D4-B2AB-0080C882687E} /v Enable /t REG_DWORD /d 0 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\TIP\{E429B25A-E5D3-4D1F-9BE3-0C608477E3A1}\LanguageProfile\0x00000404\{037B2C25-480C-4D7F-B027-D6CA6B69788A} /v Enable /t REG_DWORD /d 0 /f
-reg add HKLM\Tmp_Default\Software\Microsoft\CTF\TIP\{E429B25A-E5D3-4D1F-9BE3-0C608477E3A1}\LanguageProfile\0x00000404\{D38EFF65-AA46-4FD5-91A7-67845FB02F5B} /v Enable /t REG_DWORD /d 0 /f
-rem //-
-reg add "HKLM\Tmp_System\Keyboard Layout\Preload" /v 2 /d 00000409 /f
-reg add "HKLM\Tmp_System\Keyboard Layout\Preload" /v 1 /d 00000804 /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Default /d {00000000-0000-0000-0000-000000000000} /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Profile /d {00000000-0000-0000-0000-000000000000} /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v KeyboardLayout /t REG_DWORD /d 67699721 /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Default /d {81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E} /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Profile /d {FA550B04-5AD7-411F-A5AC-CA038EC515D7} /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v KeyboardLayout /t REG_DWORD /d 134481924 /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\HiddenDummyLayouts /v 00000804 /d 00000804 /f
-reg add HKLM\Tmp_System\Software\Microsoft\CTF\TIP\{81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E}\LanguageProfile\0x00000804\{FA550B04-5AD7-411F-A5AC-CA038EC515D7} /v Enable /t REG_DWORD /d 1 /f
 
+call :UDPATE_IME_REGISTY Tmp_Default
+call :UDPATE_IME_REGISTY Tmp_System
+
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v CLSID /d {00000000-0000-0000-0000-000000000000} /f
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v Profile /d {00000000-0000-0000-0000-000000000000} /f
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v KeyboardLayout /t REG_DWORD /d 0x4090409 /f
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v CLSID /d %_CTF_DEFAULT% /f
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v Profile /d %_CTF_PROFILE% /f
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\AssemblyItem\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31}\00000000 /v KeyboardLayout /t REG_DWORD /d 0 /f
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\SortOrder\Language /v 00000000 /d 00000804 /f
+
+rem ; Microsoft Pinyin
+set _IME_MS_PINYIN=0
+if "x%opt[IME.ms_pinyin]%"=="xtrue" set _IME_MS_PINYIN=1
+reg add HKLM\Tmp_Default\Software\Microsoft\CTF\TIP\{81D4E9C9-1D3B-41BC-9E6C-4B40BF79E35E}\LanguageProfile\0x00000804\{FA550B04-5AD7-411F-A5AC-CA038EC515D7} /v Enable /t REG_DWORD /d %_IME_MS_PINYIN% /f
+set _IME_MS_PINYIN=
+
+rem ; Microsoft Wubi
+if "x%opt[IME.ms_wubi]%"=="xtrue" (
+    reg add HKLM\Tmp_Software\Microsoft\CTF\TIP\{6A498709-E00B-4C45-A018-8F9E4081AE40}\LanguageProfile\0x00000804\{82590C13-F4DD-44F4-BA1D-8667246FDF8E} /v Enable /t REG_DWORD /d 1 /f
+)
+
+goto :EOF
+
+:UDPATE_IME_REGISTY
+reg add "HKLM\%1\Keyboard Layout\Preload" /v 2 /d 00000409 /f
+reg add "HKLM\%1\Keyboard Layout\Preload" /v 1 /d 00000804 /f
+reg add HKLM\%1\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Default /d {00000000-0000-0000-0000-000000000000} /f
+reg add HKLM\%1\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Profile /d {00000000-0000-0000-0000-000000000000} /f
+reg add HKLM\%1\Software\Microsoft\CTF\Assemblies\0x00000409\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v KeyboardLayout /t REG_DWORD /d 0x4090409 /f
+reg add HKLM\%1\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Default /d %_CTF_DEFAULT% /f
+reg add HKLM\%1\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v Profile /d %_CTF_PROFILE% /f
+reg add HKLM\%1\Software\Microsoft\CTF\Assemblies\0x00000804\{34745C63-B2F0-4784-8B67-5E12C8701A31} /v KeyboardLayout /t REG_DWORD /d 0x8040804 /f
+reg add HKLM\%1\Software\Microsoft\CTF\HiddenDummyLayouts /v 00000804 /d 00000804 /f
+goto :EOF

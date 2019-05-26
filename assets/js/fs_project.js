477771420684557ae078a87ad05d5656a7fd533c
slore <slorelee@gmail.com>
2019-05-26 23:46:28 +0800

    fix issue that StartIsBack settings couldn't show the right preview

--- assets/js/fs_project.js
+++ assets/js/fs_project.js
@@ -4,8 +4,10 @@ var Project = {
         var project = {};
         project.name = name;
         project.path = Project.root_path + '/' + name;
+        project.uri = project.path;
         project.wb_root = $wb_root.replace(/\\/g, '/');
         project.full_path = project.wb_root + '/' + project.path;
+        project.full_uri = project.full_path;
         project.full_path = project.full_path.replace(/\//g, '\\');
         function load_file(file) {
             return load_text_file(project.path + '/' + file);

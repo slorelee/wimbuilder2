25cbcabd59bb99626e32d1385159824e315f8316
slore <slorelee@gmail.com>
2019-05-29 07:31:21 +0800

    add user, owner options for ACLRegKey macro

--- lib/macros/_ACLRegKey.cmd
+++ lib/macros/_ACLRegKey.cmd
@@ -1,17 +1,22 @@
 @echo off
 
-rem MACRO:ACLRegKey
+rem MACRO:ACLRegKey RegKey [user] [owner]
 rem call SetACL.exe to set the registry's ACL
+
 echo [MACRO]ACLRegKey %*
 
-call :GRANT_REG_RIGHT "HKLM\%~1"
+call :GRANT_REG_RIGHT "HKLM\%~1" "%~2" "%~3"
 goto :EOF
 
 :GRANT_REG_RIGHT
-SetACL.exe -on "%~1" -ot reg -actn setowner -ownr "n:Administrators" -rec yes 1>nul
+set "_ACL_User=%~2"
+set "_ACL_Owner=%~3"
+if "x%_ACL_User%"=="x" set _ACL_User=Administrators
+if "x%_ACL_Owner%"=="x"  set _ACL_Owner=Administrators
+SetACL.exe -on "%~1" -ot reg -actn setowner -ownr "n:%_ACL_Owner%" -rec yes 1>nul
 if ERRORLEVEL 1 set GetLastError=1
 if not "%GetLastError%"=="0" goto :EOF
-SetACL.exe -on "%~1" -ot reg -actn ace -ace "n:Administrators;p:full" -rec yes 1>nul
+SetACL.exe -on "%~1" -ot reg -actn ace -ace "n:%_ACL_User%;p:full" -rec yes 1>nul
 if ERRORLEVEL 1 set GetLastError=1
 goto :EOF
 

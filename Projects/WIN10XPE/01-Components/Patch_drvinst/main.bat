ee42d5ecf3a78d4774cc9b3d302fa63fc93273e4
slore <slorelee@gmail.com>
2019-06-13 00:14:31 +0800

    update drvinst patcher for 20h1

--- Projects/WIN10XPE/01-Components/Patch_drvinst/main.bat
+++ Projects/WIN10XPE/01-Components/Patch_drvinst/main.bat
@@ -13,29 +13,32 @@ if "x%opt[patch_drvinst.install_test_driver]%"=="xtrue" (
   xcopy /E /Y X\*.* "%X%\"
 )
 
-set "PATCH_TMP=%Temp%"
 rem set PATCH_MODE=local
 if "%PATCH_MODE%"=="local" (
   set WB_PE_VER=10.0.18323
   set VER[3]=18323
   set WB_PE_ARCH=x64
   set X_SYS=.
-  set PATCH_TMP=.
 )
 rem use JFX's generic patcher
 rem http://theoven.org/index.php?topic=2768.0 (>= v1.0.1)
 rem http://theoven.org/index.php?topic=2726.msg31375#msg31375 (v1.0.0)
+
+if %VER[3]% GTR 18908  goto :AssemblePatch
+
 DrvInstPatch_%WB_PE_ARCH%.exe p "%X_SYS%\drvinst.exe"
+if %errorlevel% NEQ 0 (
+  echo Try Assemble Patch ...
+  goto :AssemblePatch
+)
 if "%PATCH_MODE%"=="local" pause
 goto :EOF
 
-
-
-
-
+:AssemblePatch
 set VER_NAME=%WB_PE_VER%
 if %VER[3]% GTR 17000 set VER_NAME=win10.rs4later
 if %VER[3]% GTR 17700 set VER_NAME=win10.rs5later
+if %VER[3]% GTR 18908 set VER_NAME=win10.18908later
 call :Drvinst_%VER_NAME%_%WB_PE_ARCH%
 if "%PATCH_MODE%"=="local" pause
 goto :EOF
@@ -60,9 +63,21 @@ goto :EOF
 call :PATCH 8BD8 85DB 750A 8B4D
 goto :EOF
 
+:Drvinst_win10.18908later_x64
+call :FULL_PATH 8BF0_85C0_7447 33C0_8BF0_EB47
+goto :EOF
+
+:Drvinst_win10.18908later_x86
+call :FULL_PATH 8BF0_85C0_7447 33C0_8BF0_EB47
+goto :EOF
+
+:FULL_PATH
+binmay.exe -v -s "%1" -r "%2" -u "%X_SYS%\drvinst.exe"
+goto :PATCH_CONFIRM
+
 :PATCH
-binmay.exe -v -s "%1%2%3%4" -r "33C0%19090%4" -i "%X_SYS%\drvinst.exe" -o "%PATCH_TMP%\drvinst_patched.exe"
-fc /b "%X_SYS%\drvinst.exe" "%PATCH_TMP%\drvinst_patched.exe"
-if not "x%PATCH_TMP%"=="x." (
-  copy /y "%PATCH_TMP%\drvinst_patched.exe" "%X_SYS%\drvinst.exe"
-)
+binmay.exe -v -s "%1%2%3%4" -r "33C0%19090%4" -u "%X_SYS%\drvinst.exe"
+:PATCH_CONFIRM
+fc /b "%X_SYS%\drvinst.exe.org" "%X_SYS%\drvinst.exe"
+del /q "%X_SYS%\drvinst.exe.org"
+

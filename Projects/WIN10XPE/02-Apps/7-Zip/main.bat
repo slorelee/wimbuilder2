67e23086b0b19180e61af8e87cda2b70f1f68408
slore <slorelee@gmail.com>
2019-06-21 07:28:33 +0800

    update 7-Zip\main.bat

--- Projects/WIN10XPE/02-Apps/7-Zip/main.bat
+++ Projects/WIN10XPE/02-Apps/7-Zip/main.bat
@@ -9,8 +9,11 @@ rem call Shortcuts "StartMenu,Desktop,PintTaskbar,PinStartMenu"
 goto :EOF
 
 :REMOVE_LANGFILES
-if "%WB_PE_LANG%"=="zh-CN" set _7z_lang=zh-cn
 if "%WB_PE_LANG%"=="fr-FR" set _7z_lang=fr
+if "%WB_PE_LANG%"=="ja-JP" set _7z_lang=ja
+if "%WB_PE_LANG%"=="ko-KR" set _7z_lang=ko
+if "%WB_PE_LANG%"=="zh-CN" set _7z_lang=zh-cn
+if "%WB_PE_LANG%"=="zh-TW" set _7z_lang=zh-tw
 
 if not "x%_7z_lang%"=="x" (
   ren "%X%\Program Files\7-Zip\Lang\%_7z_lang%.txt" %_7z_lang%.ttt

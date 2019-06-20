726e21bff03e1b6bebeea51bc0e58335cc5e73ad
slore <slorelee@gmail.com>
2019-06-21 07:20:24 +0800

    update 7-Zip\main.bat with new macro(s)

--- Projects/WIN10XPE/02-Apps/7-Zip/main.bat
+++ Projects/WIN10XPE/02-Apps/7-Zip/main.bat
@@ -1,6 +1,20 @@
 call V2X 7-Zip -Extract "7z*-%_V_ARCH%.exe" "%X_PF%\7-Zip\"
 copy /y 7z-Register.reg "%X_PF%\7-Zip\"
 
-rem call Shortcuts "StartMenu,Desktop,PintTaskbar,PinStartMenu"
-rem call LoadShell "X:\Program Files\7-Zip\7z-Register.reg"
+call :REMOVE_LANGFILES
 
+call Link "#pDesktop#p\7-zip" "#pProgramFiles#p\7-zip\7zFM.exe"
+call RunBeforeShell "reg import #qX:\Program Files\7-Zip\7z-Register.reg#q"
+rem call Shortcuts "StartMenu,Desktop,PintTaskbar,PinStartMenu"
+goto :EOF
+
+:REMOVE_LANGFILES
+if "%WB_PE_LANG%"=="zh-CN" set _7z_lang=zh-cn
+if "%WB_PE_LANG%"=="fr-FR" set _7z_lang=fr
+
+if not "x%_7z_lang%"=="x" (
+  ren "%X%\Program Files\7-Zip\Lang\%_7z_lang%.txt" %_7z_lang%.ttt
+  del "%X%\Program Files\7-Zip\Lang\*.txt"
+  ren "%X%\Program Files\7-Zip\Lang\%_7z_lang%.ttt" %_7z_lang%.txt
+)
+set _7z_lang=

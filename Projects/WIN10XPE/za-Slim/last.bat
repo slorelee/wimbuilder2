3fd468c2b66a414ca8b9cf928eeb7187889ae8c5
slore <slorelee@gmail.com>
2019-06-22 22:34:16 +0800

    fix issue that lite preset can't boot into the Desktop

--- Projects/WIN10XPE/za-Slim/last.bat
+++ Projects/WIN10XPE/za-Slim/last.bat
@@ -2,6 +2,7 @@ if exist "%X_SYS%\pecmd.ini" call :UPDATE_PECMD
 
 if "x%opt[slim.ultra]%"=="xtrue" (
     call TextReplace "%X_SYS%\winpeshl.ini" "wpeinit.exe" "cmd.exe,/c"
+    call TextReplace "%X_PEMaterial%\winpeshl.ini" "wpeinit.exe" "cmd.exe,/c"
 )
 
 if "x%opt[system.workgroup]%"=="x" (

949c2e4aebdc651c0571f0fb7728b16a2de5f304
slore <slorelee@gmail.com>
2019-06-29 22:35:33 +0800

    update _ACLRegKey macro

--- lib/macros/_ACLRegKey.cmd
+++ lib/macros/_ACLRegKey.cmd
@@ -13,11 +13,15 @@ set "_ACL_User=%~2"
 set "_ACL_Owner=%~3"
 if "x%_ACL_User%"=="x" set _ACL_User=Administrators
 if "x%_ACL_Owner%"=="x"  set _ACL_Owner=Administrators
-SetACL.exe -on "%~1" -ot reg -actn setowner -ownr "n:%_ACL_Owner%" -rec yes 1>nul
-if ERRORLEVEL 1 set GetLastError=1
+set GetLastError=0
+if not "%_ACL_Owner%"=="-" (
+  SetACL.exe -on "%~1" -ot reg -actn setowner -ownr "n:%_ACL_Owner%" -rec yes
+  if ERRORLEVEL 1 set GetLastError=1
+)
 if not "%GetLastError%"=="0" goto :EOF
-SetACL.exe -on "%~1" -ot reg -actn ace -ace "n:%_ACL_User%;p:full" -rec yes 1>nul
-if ERRORLEVEL 1 set GetLastError=1
+
+if not "%_ACL_User%"=="-" (
+  SetACL.exe -on "%~1" -ot reg -actn ace -ace "n:%_ACL_User%;p:full" -rec yes
+  if ERRORLEVEL 1 set GetLastError=1
+)
 goto :EOF
-
-

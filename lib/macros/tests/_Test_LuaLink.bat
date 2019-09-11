7d9176c4cc1a4bf4a85a0c872109c40048e853b3
slore <slorelee@gmail.com>
2019-09-11 15:59:09 +0800

    add tests for LuaLink, LuaPin macros

---
+++ lib/macros/tests/_Test_LuaLink.bat
@@ -0,0 +1,27 @@
+@echo off
+cd /d "%~dp0"
+cd ..
+
+if not exist tests\tmp md tests\tmp
+copy /y tests\Shortcuts.lua tests\tmp\
+
+call LuaLink -init "%~dp0tmp\Shortcuts.lua"
+
+call LinkToDesktop "Text Editor.lnk" notepad.exe
+call LinkToDesktop WinXShell.lnk "X:\Program Files\WinXShell\WinXShell.exe"
+call LinkToDesktop Tools\PENetwork.lnk "%%%%%%%%ProgramFiles%%%%%%%%\PENetwork\PENetwork.exe"
+call LinkToDesktop -paramlist "UI_Shutdown.lnk" "[[%%%%%%%%ProgramFiles%%%%%%%%\WinXShell\WinXShell.exe]], '-ui -jcfg wxsUI\\UI_Shutdown.zip\\full.jcfg -blur 5', 'shell32.dll', 27"
+
+call LinkToStartMenu "SystemTools\Reg Editor.lnk" regedit.exe
+call LinkToStartMenu "File Manager.lnk" explorer.exe
+call LinkToStartMenu Tools\PENetwork.lnk "%%%%%%%%ProgramFiles%%%%%%%%\PENetwork\PENetwork.exe"
+call LinkToStartMenu -paramlist "WinXShell\UI_Shutdown.lnk" "[[%%%%%%%%ProgramFiles%%%%%%%%\WinXShell\WinXShell.exe]], '-ui -jcfg wxsUI\\UI_Shutdown.zip\\full.jcfg -blur 5', 'shell32.dll', 27"
+
+rem use #p than %%%%%%%%
+call LinkToDesktop "7-Zip.lnk" "#pProgramFiles#p\7-Zip\7zFM.exe"
+call LinkToStartMenu "7-Zip\7-Zip File Manager.lnk" "#pProgramFiles#p\7-Zip\7zFM.exe"
+
+call LuaLink -done
+
+pause
+
---
+++ lib/macros/tests/_Test_LuaPin.bat
@@ -0,0 +1,27 @@
+@echo off
+cd /d "%~dp0"
+cd ..
+
+if not exist tests\tmp md tests\tmp
+copy /y tests\PinShortcuts.lua tests\tmp\
+
+call LuaPin -init "%~dp0tmp\PinShortcuts.lua"
+
+call PinToTaskbar notepad.exe
+call PinToTaskbar "X:\Program Files\WinXShell\WinXShell.exe"
+call PinToTaskbar "%%%%%%%%ProgramFiles%%%%%%%%\PENetwork\PENetwork.exe"
+call PinToTaskbar -paramlist "[[%%%%%%%%ProgramFiles%%%%%%%%\WinXShell\WinXShell.exe]], 'UI_Shutdown', '-ui -jcfg wxsUI\\UI_Shutdown.zip\\full.jcfg -blur 5', 'shell32.dll', 27"
+
+call PinToStartMenu regedit.exe
+call PinToStartMenu "X:\Program Files\WinXShell\WinXShell.exe"
+call PinToStartMenu "%%%%%%%%ProgramFiles%%%%%%%%\PENetwork\PENetwork.exe"
+call PinToStartMenu -paramlist "[[%%%%%%%%ProgramFiles%%%%%%%%\WinXShell\WinXShell.exe]], 'wxsExplorer', nil, 'shell32.dll', 27"
+
+rem use #p than %%%%%%%%
+call PinToTaskbar "#pProgramFiles#p\7-Zip\7zFM.exe"
+call PinToStartMenu "#pProgramFiles#p\7-Zip\7zFM.exe"
+
+call LuaPin -done
+
+pause
+

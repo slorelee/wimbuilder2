8f649e8826189374a56fb92e99a4a8790ba79060
slore <slorelee@gmail.com>
2019-09-23 00:47:34 +0800

    add _CustomFiles_\MyTheme sample

---
+++ Projects/WIN10XPE/_CustomFiles_/MyTheme/last.bat
@@ -0,0 +1,6 @@
+if "x%opt[mytheme.theme_id]%"=="x" goto :EOF
+
+echo [Example]Theme Id: %opt[mytheme.theme_id]%
+
+echo [Example]7za.exe x "%~dp0Web%opt[mytheme.theme_id]%.7z" -y -aos -o"%X_WIN%\"
+echo [Example]copy /y startup%opt[mytheme.theme_id]%.wav "%X_WIN%\Media\startup.wav"

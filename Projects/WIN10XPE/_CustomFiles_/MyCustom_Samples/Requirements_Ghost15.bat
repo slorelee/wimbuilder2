b7b94b9e6cf7d2336b1a71017f934cd2f1bcc582
slore <slorelee@gmail.com>
2019-10-11 22:31:52 +0800

    add Requirements_Ghost15.bat

---
+++ Projects/WIN10XPE/_CustomFiles_/MyCustom_Samples/Requirements_Ghost15.bat
@@ -0,0 +1,13 @@
+call AddFiles %0 :end_files
+goto :end_files
+
+;ghost32.exe
+@\Windows\SysWOW64\
+snmpapi.dll
+
+;ghost15 SRD - Backup/Restore
+clusapi.dll,mfc42u.dll,resutils.dll
+@\Windows\System32\
+IEAdvpack.dll
+
+:end_files

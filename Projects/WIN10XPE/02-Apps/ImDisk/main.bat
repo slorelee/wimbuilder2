5333f6062579c0005a85319e063d3b5b3732eb58
slore <slorelee@gmail.com>
2019-11-09 11:14:21 +0800

    add options for ImDisk

--- Projects/WIN10XPE/02-Apps/ImDisk/main.bat
+++ Projects/WIN10XPE/02-Apps/ImDisk/main.bat
@@ -4,3 +4,8 @@ if exist ImDisk_Settings.%WB_PE_LANG%.reg (
 )
 
 copy /y ImDisk_Install.bat "%X_Startup%\BeforeShell\"
+
+if not "x%opt[imdisk.ramdisk]%"=="xtrue" goto :EOF
+(
+  echo imdisk.exe -a -t vm -s %opt[imdisk.disk_size]%M -m %opt[imdisk.drive_letter]% -p "/fs:ntfs /v:RamDisk /q /c /y"
+) >> "%X_Startup%\BeforeShell\ImDisk_Install.bat"

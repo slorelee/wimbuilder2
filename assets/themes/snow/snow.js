c99d1a6e4af893cb51feada1a40af2aaca1489c3
slore <slorelee@gmail.com>
2019-12-10 23:22:32 +0800

    add snow theme

---
+++ assets/themes/snow/snow.js
@@ -0,0 +1,129 @@
+(function() {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(callback) {
+        window.setTimeout(callback, 1000);
+    };
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+
+var flakes = [],
+    canvas = document.getElementById("theme_canvas"),
+    ctx = canvas.getContext("2d"),
+    flakeCount = 200,
+    mX = -100,
+    mY = -100
+
+    resize_theme_canvas();
+
+function snow_draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = 0; i < flakeCount; i++) {
+        var flake = flakes[i],
+            x = mX,
+            y = mY,
+            minDist = 150,
+            x2 = flake.x,
+            y2 = flake.y;
+
+        var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
+            dx = x2 - x,
+            dy = y2 - y;
+
+        if (dist < minDist) {
+            var force = minDist / (dist * dist),
+                xcomp = (x - x2) / dist,
+                ycomp = (y - y2) / dist,
+                deltaV = force / 2;
+
+            flake.velX -= deltaV * xcomp;
+            flake.velY -= deltaV * ycomp;
+
+        } else {
+            flake.velX *= .98;
+            if (flake.velY <= flake.speed) {
+                flake.velY = flake.speed
+            }
+            flake.velX += Math.cos(flake.step += .05) * flake.stepSize;
+        }
+
+        ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
+        flake.y += flake.velY;
+        flake.x += flake.velX;
+            
+        if (flake.y >= canvas.height || flake.y <= 0) {
+            snow_reset(flake);
+        }
+
+
+        if (flake.x >= canvas.width || flake.x <= 0) {
+            snow_reset(flake);
+        }
+
+        ctx.beginPath();
+        ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+    requestAnimationFrame(snow_draw);
+};
+
+function snow_reset(flake) {
+    flake.x = Math.floor(Math.random() * canvas.width);
+    flake.y = 0;
+    flake.size = (Math.random() * 3) + 2;
+    flake.speed = (Math.random() * 1) + 0.5;
+    flake.velY = flake.speed;
+    flake.velX = 0;
+    flake.opacity = (Math.random() * 0.5) + 0.3;
+}
+
+function snow_init() {
+    for (var i = 0; i < flakeCount; i++) {
+        var x = Math.floor(Math.random() * canvas.width),
+            y = Math.floor(Math.random() * canvas.height),
+            size = (Math.random() * 3) + 2,
+            speed = (Math.random() * 1) + 0.5,
+            opacity = (Math.random() * 0.5) + 0.3;
+
+        flakes.push({
+            speed: speed,
+            velY: speed,
+            velX: 0,
+            x: x,
+            y: y,
+            size: size,
+            stepSize: (Math.random()) / 30,
+            step: 0,
+            opacity: opacity
+        });
+    }
+
+    snow_draw();
+};
+
+canvas.addEventListener("mousemove", function(e) {
+    mX = e.clientX,
+    mY = e.clientY
+});
+
+function resize_theme_canvas(dy) {
+    var dw = window.innerWidth - document.body.clientWidth;
+    var ch = document.body.offsetHeight;
+    if (!dy) dy = 0;
+    if (ch < window.innerHeight) ch = window.innerHeight;
+    if (dw > 0) ch += (15 + dy);
+    canvas.width = window.innerWidth - dw;
+    canvas.height = ch;
+}
+
+window.addEventListener("resize",function(){
+    resize_theme_canvas();
+})
+
+snow_init();
+
+$('.pure-menu-link').click(function(){
+    resize_theme_canvas(-15);
+    resize_theme_canvas();
+});

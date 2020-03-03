0f8fc8f6a90c5b08cce8629074a6719ca66bfe2d
slore <slorelee@gmail.com>
2020-03-03 15:49:55 +0800

    add 128GB option for FBWF cache size

--- Projects/WIN10XPE/00-Configures/System/last.bat
+++ Projects/WIN10XPE/00-Configures/System/last.bat
@@ -2,15 +2,19 @@
 call ComputerName.bat
 reg add "HKLM\Tmp_Software\Microsoft\Windows NT\CurrentVersion\ProfileList\S-1-5-18" /v ProfileImagePath /d X:\Users\Default /f
 
-
-rem Fbwf Cache Size. Limited to 1024 Mb with x86
+@echo on
 set _fbwf_size=%opt[config.fbwf.cache]%
 if "x%_fbwf_size%"=="x" set _fbwf_size=1024
+if "x%_fbwf_size%"=="x128GB" goto :USE_WES_FBWF
+
+rem Fbwf Cache Size. Limited to 1024 Mb with x86
 if %_fbwf_size% GTR 1024 (
-  if "%WB_PE_ARCH%"=="x86" set _fbwf_size=1024
+    if "%WB_PE_ARCH%"=="x86" set _fbwf_size=1024
 )
 if "%_fbwf_size%"=="4096" set /a _fbwf_size-=1
 reg add HKLM\Tmp_System\ControlSet001\Services\FBWF /v WinPECacheThreshold /t REG_DWORD /d %_fbwf_size% /f
+
+:USE_WES_FBWF
 if exist fbwf_%_fbwf_size%.cfg (
   echo Enable %_fbwf_size% MB cache size with Windows Embedded Standard's fbwf driver
   copy /y fbwf_%_fbwf_size%.cfg "%X_WIN%\fbwf.cfg"

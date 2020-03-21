a8ae11959015d5501f762fc4e38ce46aa2bcc68a
slore <slorelee@gmail.com>
2020-03-21 23:44:17 +0800

    update project(s)

--- Projects/WIN10XPE/_CustomFiles_/MyTheme/last.bat
+++ Projects/WIN10XPE/_CustomFiles_/MyTheme/last.bat
@@ -2,5 +2,5 @@ if "x%opt[mytheme.theme_id]%"=="x" goto :EOF
 
 echo [Example]Theme Id: %opt[mytheme.theme_id]%
 
-echo [Example]7za.exe x "%~dp0Web%opt[mytheme.theme_id]%.7z" -y -aos -o"%X_WIN%\"
+echo [Example]7z.exe x "%~dp0Web%opt[mytheme.theme_id]%.7z" -y -aos -o"%X_WIN%\"
 echo [Example]copy /y startup%opt[mytheme.theme_id]%.wav "%X_WIN%\Media\startup.wav"

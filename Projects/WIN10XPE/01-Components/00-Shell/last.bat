22ff747c38f3ff2a23284f064a47e308265a998a
slore <slorelee@gmail.com>
2020-03-22 16:32:34 +0800

    add "shell.show_recyclebin" option

--- Projects/WIN10XPE/01-Components/00-Shell/last.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/last.bat
@@ -5,6 +5,13 @@ if not "x%opt[shell.show_thisPC]%"=="xfalse" (
     reg add "HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {20D04FE0-3AEA-1069-A2D8-08002B30309D} /t REG_DWORD /d 1 /f
 )
 
+rem show Recycle Bin on Desktop
+if not "x%opt[shell.show_recyclebin]%"=="xfalse" (
+    reg add "HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {645FF040-5081-101B-9F08-00AA002F954E} /t REG_DWORD /d 0 /f
+) else (
+    reg add "HKLM\Tmp_Default\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {645FF040-5081-101B-9F08-00AA002F954E} /t REG_DWORD /d 1 /f
+)
+
 if not "x%opt[desktop.iconsize]%"=="x" (
     reg add "HKLM\Tmp_Default\Software\Microsoft\Windows\Shell\Bags\1\Desktop" /v IconSize /t REG_DWORD /d %opt[desktop.iconsize]% /f
 )

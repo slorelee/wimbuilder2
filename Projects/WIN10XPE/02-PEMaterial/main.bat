27fc6ed92e75ab57ba206b36cb8f3e1f605b254a
slore <slorelee@gmail.com>
2020-03-22 16:16:49 +0800

    register 7z command to environment variable (%c7z%)

--- Projects/WIN10XPE/02-PEMaterial/main.bat
+++ Projects/WIN10XPE/02-PEMaterial/main.bat
@@ -6,4 +6,6 @@ if "x%opt[material.location]%"=="x_U_" set "PEM.Loc=%WB_ROOT%\_U_\PEMaterial"
 set SysWOW64=SysWOW64
 if "x%WB_PE_ARCH%"=="xx86" set SysWOW64=System32
 
+reg add "HKLM\tmp_DEFAULT\Environment" /v c7z /d "X:\Program Files\7-Zip\7z.exe" /f
+
 call App init _Cache_

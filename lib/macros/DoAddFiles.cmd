8806513bef5aff1ce43ec621c53ec4a82e09a72d
slore <slorelee@gmail.com>
2020-03-22 16:15:21 +0800

    update DoAddFiles macro (support to extract files to specific folder)

--- lib/macros/DoAddFiles.cmd
+++ lib/macros/DoAddFiles.cmd
@@ -7,7 +7,12 @@ if "x%_WB_TMP_DIR%"=="x" goto :EOF
 
 if not exist "%_WB_TMP_DIR%\_AddFiles.txt" goto :EOF
 
-call wimextract "%WB_SRC%" %WB_SRC_INDEX% @"%_WB_TMP_DIR%\_AddFiles.txt" --dest-dir="%_WB_MNT_PATH%" --no-acls --nullglob
+set "_DEST_DIR=%_WB_MNT_PATH%"
+if not "x%~1"=="x" set "_DEST_DIR=%~1"
+
+echo --dest-dir="%_DEST_DIR%" 
+call wimextract "%WB_SRC%" %WB_SRC_INDEX% @"%_WB_TMP_DIR%\_AddFiles.txt" --dest-dir="%_DEST_DIR%" --no-acls --nullglob
+set _DEST_DIR=
 
 type nul>"%_WB_TMP_DIR%\_AddFiles.txt"
 set AddFiles_Mode=

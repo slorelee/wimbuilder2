e8f04380da4ffb925f263702af7493783275c855
slore <slorelee@gmail.com>
2020-03-27 15:21:06 +0800

    fix issue that UnMountWIM.cmd macro can't work

--- lib/macros/UnMountWIM.cmd
+++ lib/macros/UnMountWIM.cmd
@@ -2,14 +2,8 @@
 if "x%WB_PROJECT%"=="x" goto :EOF
 
 if "x%_WB_PE_WIM%"=="x" (
-    set "_WB_TAR_DIR=%Factory%\target\%WB_PROJECT%"
-    set "_WB_MNT_DIR=%_WB_TAR_DIR%\mounted"
-
-    call :GETNAME "%WB_BASE%"
-    set "_WB_PE_WIM=%_WB_TAR_DIR%\%RET_GETNAME%"
-    set _KEEP_CONSOLE=1
+    call :GETWIM
 )
-
 call _Cleanup 0
 call WIM_Exporter "%_WB_PE_WIM%"
 
@@ -17,6 +11,16 @@ echo \033[96mWIM Exported:%_WB_TAR_DIR%\build\boot.wim | cmdcolor.exe
 
 if "x%_KEEP_CONSOLE%"=="x1" pause
 
+goto :EOF
+
+
+:GETWIM
+set "_WB_TAR_DIR=%Factory%\target\%WB_PROJECT%"
+set "_WB_MNT_DIR=%_WB_TAR_DIR%\mounted"
+
+call :GETNAME "%WB_BASE%"
+set "_WB_PE_WIM=%_WB_TAR_DIR%\%RET_GETNAME%"
+set _KEEP_CONSOLE=1
 goto :EOF
 
 :GETNAME

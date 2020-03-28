b3c7be4e77f78ecd000b87bc6c0be80ea0058420
slore <slorelee@gmail.com>
2020-03-28 16:18:37 +0800

    add material.load_mode option

--- Projects/WIN10XPE/02-PEMaterial/main.bat
+++ Projects/WIN10XPE/02-PEMaterial/main.bat
@@ -3,6 +3,11 @@ set "PEM.Loc=%X%\PEMaterial"
 if "x%opt[material.location]%"=="x_ISO_" set "PEM.Loc=%WB_ROOT%\_ISO_\PEMaterial"
 if "x%opt[material.location]%"=="x_U_" set "PEM.Loc=%WB_ROOT%\_U_\PEMaterial"
 
+if "x%opt[material.load_mode]%"=="xdemand" (
+    echo.> "%X_PEMaterial%\Autoruns\PEStartup.Disabled"
+    call LinkToDesktop -paramlist "%opt[_i18n.external_material]%.lnk" "[[#pProgramFiles#p\WinXShell\WinXShell.exe]], [=[-code exec#{#'/min',[[X:\%opt[loader.PEMaterial]%\Autoruns\PEStartupMain.bat]]#}#]=], 'imageres.dll', 152"
+)
+
 set SysWOW64=SysWOW64
 if "x%WB_PE_ARCH%"=="xx86" set SysWOW64=System32
 

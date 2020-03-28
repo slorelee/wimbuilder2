77c22ed3980586c316d52fdfb45fbd2db20186bd
slore <slorelee@gmail.com>
2020-03-28 22:32:57 +0800

    remove debug log

--- lib/macros/DoAddFiles.cmd
+++ lib/macros/DoAddFiles.cmd
@@ -10,7 +10,7 @@ if not exist "%_WB_TMP_DIR%\_AddFiles.txt" goto :EOF
 set "_DEST_DIR=%_WB_MNT_PATH%"
 if not "x%~1"=="x" set "_DEST_DIR=%~1"
 
-echo --dest-dir="%_DEST_DIR%" 
+rem echo --dest-dir="%_DEST_DIR%" 
 call wimextract "%WB_SRC%" %WB_SRC_INDEX% @"%_WB_TMP_DIR%\_AddFiles.txt" --dest-dir="%_DEST_DIR%" --no-acls --nullglob
 set _DEST_DIR=
 

928d1b8e4ab91e55e080e0d16728db6ff9238008
slore <slorelee@gmail.com>
2020-04-17 00:54:05 +0800

    update 02-PEMaterial/main.bat,last.bat

--- Projects/WIN10XPE/02-PEMaterial/last.bat
+++ Projects/WIN10XPE/02-PEMaterial/last.bat
@@ -1,1 +1,4 @@
 set Downloads
+
+if not "%PEM.Loc%"=="%X%\PEMaterial" goto :EOF
+move /y "%PEM.Loc%\Program Files" "%X%\Program Files"
--- Projects/WIN10XPE/02-PEMaterial/main.bat
+++ Projects/WIN10XPE/02-PEMaterial/main.bat
@@ -1,4 +1,4 @@
-
+@echo on
 set "PEM.Loc=%X%\PEMaterial"
 if "x%opt[material.location]%"=="x_ISO_" set "PEM.Loc=%WB_ROOT%\_ISO_\PEMaterial"
 if "x%opt[material.location]%"=="x_U_" set "PEM.Loc=%WB_ROOT%\_U_\PEMaterial"
@@ -14,3 +14,8 @@ if "x%WB_PE_ARCH%"=="xx86" set SysWOW64=System32
 reg add "HKLM\tmp_DEFAULT\Environment" /v c7z /d "X:\Program Files\7-Zip\7z.exe" /f
 
 call App init _Cache_
+
+rem templates
+if not exist "%PEM.Loc%\Installers\" mkdir "%PEM.Loc%\Installers\"
+if not exist "%PEM.Loc%\PortableApps\" mkdir "%PEM.Loc%\PortableApps\"
+if not exist "%PEM.Loc%\Program Files\" mkdir "%PEM.Loc%\Program Files\"

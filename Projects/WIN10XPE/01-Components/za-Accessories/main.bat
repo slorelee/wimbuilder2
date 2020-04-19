82cf5523f8012783b4597d164ad0c0db15fb06bf
slore <slorelee@gmail.com>
2020-04-19 22:11:21 +0800

    add WordPad

--- Projects/WIN10XPE/01-Components/za-Accessories/main.bat
+++ Projects/WIN10XPE/01-Components/za-Accessories/main.bat
@@ -1,3 +1,9 @@
+if "x%opt[component.wordpad]%"=="xtrue" (
+  call AddFiles "\Program Files\Windows NT\Accessories\"
+  call RegCopyEx Classes ".docx,.odt,.rtf,docxfile,odtfile,rtffile"
+  rem call RegCopyEx Classes "textfile,Wordpad.Document.1,Application\wordpad.exe"
+)
+
 if "x%opt[component.mspaint]%"=="xtrue" call _mspaint.bat
 
 if "x%opt[component.winphotoviewer]%"=="xtrue" (

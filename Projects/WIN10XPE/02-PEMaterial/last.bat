7ccb17d537afe91c2163497fc476e4f77fcd7a10
slore <slorelee@gmail.com>
2020-04-27 01:31:56 +0800

    update 02-PEMaterial/last.bat

--- Projects/WIN10XPE/02-PEMaterial/last.bat
+++ Projects/WIN10XPE/02-PEMaterial/last.bat
@@ -1,4 +1,11 @@
 set Downloads
 
 if not "%PEM.Loc%"=="%X%\PEMaterial" goto :EOF
-move /y "%PEM.Loc%\Program Files" "%X%\Program Files"
+if not exist "%PEM.Loc%\Program Files\" goto :EOF
+
+move /y "%PEM.Loc%\Program Files\*.*" "%X%\Program Files\"
+for /f "delims=" %%i in ('dir /ad /b "%PEM.Loc%\Program Files\"') do (
+  move /y "%PEM.Loc%\Program Files\%%i" "%X%\Program Files\"
+)
+
+rd /s /q "%PEM.Loc%\Program Files\"

07a0038055dc4889ec3638d93971a02284a6c1cb
slore <slorelee@gmail.com>
2020-05-05 19:42:51 +0800

    update Patch_drvinst/main.bat for new version of windows

--- Projects/WIN10XPE/01-Components/Patch_drvinst/main.bat
+++ Projects/WIN10XPE/01-Components/Patch_drvinst/main.bat
@@ -25,6 +25,7 @@ rem http://theoven.org/index.php?topic=2768.0 (>= v1.0.1)
 rem http://theoven.org/index.php?topic=2726.msg31375#msg31375 (v1.0.0)
 
 if %VER[3]% LSS 15063 goto :AssemblePatch
+if %VER[3]% GEQ 19041 goto :AssemblePatch
 
 DrvInstPatch_%WB_ARCH%.exe p "%X_SYS%\drvinst.exe"
 if %errorlevel% NEQ 1 (
@@ -40,6 +41,7 @@ set VER_NAME=%WB_PE_VER%
 if %VER[3]% GTR 17000 set VER_NAME=win10.rs4later
 if %VER[3]% GTR 17700 set VER_NAME=win10.rs5later
 if %VER[3]% GTR 18908 set VER_NAME=win10.18908later
+if %VER[3]% GEQ 19041 set VER_NAME=win10.20h1later
 call :Drvinst_%VER_NAME%_%WB_PE_ARCH%
 if "%PATCH_MODE%"=="local" pause
 goto :EOF
@@ -74,6 +76,14 @@ goto :EOF
 
 :Drvinst_win10.18908later_x86
 call :FULL_PATCH 8BD8_85DB_742B_53 33C0_8BD8_EB2B_53
+goto :EOF
+
+:Drvinst_win10.20h1later_x64
+call :FULL_PATCH 8BF0_85C0_7423 33C0_8BF0_EB23
+goto :EOF
+
+:Drvinst_win10.20h1later_x86
+call :FULL_PATCH 8BD8_85DB_741B_53 33C0_8BD8_EB1B_53
 goto :EOF
 
 :FULL_PATCH

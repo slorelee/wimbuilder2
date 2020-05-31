93a07b167e33337b3725c0cd2445f66ef0d14991
slore <slorelee@gmail.com>
2020-05-31 16:58:11 +0800

    make Notepad.lnk be displayed in localized name

--- Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/main.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/main.bat
@@ -4,9 +4,10 @@ goto :end_files
 \Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessibility\desktop.ini
 \Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\desktop.ini
 \Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\Desktop.ini
-
+\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Notepad.lnk
 :end_files
 
+del /s /q "%X%\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Accessories\Notepad.lnk"
 
 if "x%opt[shell.startmenu]%"=="x" set opt[shell.startmenu]=auto
 if "%opt[shell.startmenu]%"=="auto" (

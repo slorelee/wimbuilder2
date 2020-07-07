eae93c73cf1b61aae5d7968c83b934e16fdbb849
slore <slorelee@gmail.com>
2020-07-07 13:54:35 +0900

    add App_MPC-BE.bat

---
+++ Projects/WIN10XPE/02-PEMaterial/03-Media/App_MPC-BE.bat
@@ -0,0 +1,10 @@
+:Requirements
+call AddFiles "%~f0" :end_files
+goto :end_files
+@\Windows\System32\
+devenum.dll,evr.dll,l3codecp.acm,mfplat.dll
+MP3DMOD.DLL,MP43DECD.DLL,MP4SDECD.DLL,Mpeg2Data.ax
+mpg2splt.ax,MPG4DECD.DLL,msmpeg2adec.dll,msmpeg2vdec.dll,qasf.dll,unregmp2.exe
+wmdrmsdk.dll,wmploc.DLL,WMSPDMOD.DLL,WMVCORE.DLL,WMVDECOD.DLL,wmvdspa.dll,WMVSDECD.DLL
+:end_files
+goto :EOF

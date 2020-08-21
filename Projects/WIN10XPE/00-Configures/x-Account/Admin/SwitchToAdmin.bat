73d2c16f4adc6cc937a34562f2b957a76ebe8adb
slore <slorelee@gmail.com>
2020-08-21 09:31:06 +0800

    update SwitchToAdmin.bat

--- Projects/WIN10XPE/00-Configures/x-Account/Admin/SwitchToAdmin.bat
+++ Projects/WIN10XPE/00-Configures/x-Account/Admin/SwitchToAdmin.bat
@@ -109,6 +109,8 @@ reg add HKLM\Tmp_System\ControlSet001\Services\TrustedInstaller /v Start /t REG_
 call RegCopy "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication"
 call RegCopy "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 
+reg add "HKLM\Tmp_Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Associations" /v "LowRiskFileTypes" /d ".com;.exe;.bat;.cmd;" /f
+
 if %opt[account.autologon_countdown]% GTR 0 (
   rem Enable Mouse Cursor (EnableCursorSuppression=0) or use Exec = Winpeshl.exe in PecmdAdmin.ini
   reg add HKLM\Tmp_Software\Microsoft\Windows\CurrentVersion\Policies\System /v EnableCursorSuppression /t REG_DWORD /d 0 /f

d2fc85a1ea3b1322374dc171bb7cb6bc8ca82497
slore <slorelee@gmail.com>
2020-10-23 00:53:10 +0800

    fix typo

--- Projects/WIN10XPE/za-Slim/main.bat
+++ Projects/WIN10XPE/za-Slim/main.bat
@@ -153,7 +153,7 @@ if "x%opt[slim.winboot]%"=="xtrue" (
     del /f /a /q "%X_WIN%\Boot\Fonts\jpn_boot.ttf"
     del /f /a /q "%X_WIN%\Boot\Fonts\kor_boot.ttf"
     del /f /a /q "%X_WIN%\Boot\Fonts\msjh_boot.ttf"
-    del /f /a /q "%X_WIN%\Boot\Fonts\msjhn_boot.ttf.ttf"
+    del /f /a /q "%X_WIN%\Boot\Fonts\msjhn_boot.ttf"
     rd /s /q "%X_WIN%\Boot\PXE\*.com"
     rd /s /q "%X_WIN%\Boot\PXE\*.n12"
     rd /s /q "%X_WIN%\Boot\PXE\WdsConfig.inf"

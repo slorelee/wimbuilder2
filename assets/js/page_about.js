f262fb2f837cd39fbddc271803af47413ee8ac88
slore <slorelee@gmail.com>
2020-11-14 11:54:44 +0800

    add info message for 'Update' button

--- assets/js/page_about.js
+++ assets/js/page_about.js
@@ -1,4 +1,10 @@
 function update_me() {
+  if (!fso.FileExists("_Factory_\\tmp\\remote.md5")) {
+    var msg = i18n_t('Will update from %s, you can change the update source in [Settings] page.');
+    msg = msg.replace('%s', $("#ui_update option:selected").text());
+    alert(msg);
+  }
+
   app_env_init();
   Run('_Updater.cmd')
 }

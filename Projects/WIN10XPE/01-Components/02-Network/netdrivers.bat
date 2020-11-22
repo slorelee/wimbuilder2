06f74daccb03574f9f6a9ab30c313d6a4e353bef
slore <slorelee@gmail.com>
2020-11-22 13:54:13 +0800

    refactoring (01-Components/02-Network)

--- Projects/WIN10XPE/01-Components/02-Network/netdrivers.bat
+++ Projects/WIN10XPE/01-Components/02-Network/netdrivers.bat
@@ -3,22 +3,26 @@ if not "x%opt[network.builtin_drivers]%"=="xtrue" goto :EOF
 
 set AddFiles_Mode=merge
 
-if "%WB_PE_ARCH%"=="x64" (
-  call AddDrivers "athw8x.inf,netathr10x.inf,netathrx.inf,netbc63a.inf"
-  call AddDrivers "netwbw02.inf,netwew00.inf,netwew01.inf,netwlv64.inf,netwns64.inf,netwsw00.inf,netwtw04.inf"
-) else (
-  call AddDrivers "athw8.inf,netathr.inf,netathr10.inf,netbc63.inf"
-  call AddDrivers "netwbn02.inf,netwen00.inf,netwen01.inf,netwlv32.inf,netwns32.inf,netwsn00.inf,netwtn04.inf"
-)
-call AddDrivers "netbc64.inf,netrtwlane.inf,netrtwlane_13.inf,netrtwlanu.inf"
+call :NETDRIVERS_%WB_PE_ARCH%
+goto :END_NETDRIVERS
 
-if %VER[3]% LEQ 17700 goto :END_NETDRIVERS
+:NETDRIVERS_x64
+call AddDrivers "athw8x.inf,netathr10x.inf,netathrx.inf,netbc63a.inf"
+call AddDrivers "netwbw02.inf,netwew00.inf,netwew01.inf,netwlv64.inf,netwns64.inf,netwsw00.inf,netwtw04.inf"
 
-if "%WB_PE_ARCH%"=="x64" (
+if %VER[3]% GTR 17700 (
   call AddDrivers "netwtw02,netwtn06.inf"
-) else (
+)
+goto :EOF
+
+:NETDRIVERS_x86
+call AddDrivers "athw8.inf,netathr.inf,netathr10.inf,netbc63.inf"
+call AddDrivers "netwbn02.inf,netwen00.inf,netwen01.inf,netwlv32.inf,netwns32.inf,netwsn00.inf,netwtn04.inf"
+
+if %VER[3]% GTR 17700 (
   call AddDrivers netwtn02.inf
 )
+goto :EOF
 
 :END_NETDRIVERS
 

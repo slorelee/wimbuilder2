01bb829623bf22a966c7d229ef1238d49f2fccf2
slore <slorelee@gmail.com>
2020-11-22 20:06:53 +0800

    update netdrivers.bat

--- Projects/WIN10XPE/01-Components/02-Network/netdrivers.bat
+++ Projects/WIN10XPE/01-Components/02-Network/netdrivers.bat
@@ -7,21 +7,23 @@ call :NETDRIVERS_%WB_PE_ARCH%
 goto :END_NETDRIVERS
 
 :NETDRIVERS_x64
-call AddDrivers "athw8x.inf,netathr10x.inf,netathrx.inf,netbc63a.inf"
-call AddDrivers "netwbw02.inf,netwew00.inf,netwew01.inf,netwlv64.inf,netwns64.inf,netwsw00.inf,netwtw04.inf"
+call AddDrivers "athw8x.inf,bcmwdidhdpcie.inf,mrvlpcie8897.inf"
+call AddDrivers "net8185.inf,net8187bv64.inf,net8187se64.inf,net8192se64.inf,net8192su64.inf,net819xp.inf"
+call AddDrivers "netathr10x.inf,netathrx.inf,netbc63a.inf,netbc64.inf,netr28ux.inf,netr28x.inf,netr7364.inf"
+call AddDrivers "netrtwlane.inf,netrtwlane01.inf,netrtwlane_13.inf,netrtwlans.inf,netrtwlanu.inf"
+call AddDrivers "netwbw02.inf,netwew00.inf,netwew01.inf,netwlv64.inf,netwns64.inf"
+call AddDrivers "netwsw00.inf,netwtw02.inf,netwtw04.inf,netwtw06.inf,netwtw08.inf"
 
-if %VER[3]% GTR 17700 (
-  call AddDrivers "netwtw02,netwtn06.inf"
-)
 goto :EOF
 
 :NETDRIVERS_x86
-call AddDrivers "athw8.inf,netathr.inf,netathr10.inf,netbc63.inf"
-call AddDrivers "netwbn02.inf,netwen00.inf,netwen01.inf,netwlv32.inf,netwns32.inf,netwsn00.inf,netwtn04.inf"
+call AddDrivers "athw8.inf,bcmwdidhdpcie.inf,mrvlpcie8897.inf"
+call AddDrivers "net8185.inf,net8187bv32.inf,net8187se86.inf,net8192se32.inf,net8192su32.inf,net819xp.inf"
+call AddDrivers "netathr.inf,netathr10.inf,netbc63.inf,netbc64.inf,netr28.inf,netr28u.inf,netr73.inf"
+call AddDrivers "netrtwlane.inf,netrtwlane01.inf,netrtwlane_13.inf,netrtwlans.inf,netrtwlanu.inf"
+call AddDrivers "netwbn02.inf,netwen00.inf,netwen01.inf,netwlv32.inf,netwns32.inf"
+call AddDrivers "netwsn00.inf,netwtn02.inf,netwtn04.inf"
 
-if %VER[3]% GTR 17700 (
-  call AddDrivers netwtn02.inf
-)
 goto :EOF
 
 :END_NETDRIVERS

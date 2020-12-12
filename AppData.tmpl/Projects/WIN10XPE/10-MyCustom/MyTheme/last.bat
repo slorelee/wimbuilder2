26898c57bbae80e9cd69d200c6edc431fba79a8d
slore <slorelee@gmail.com>
2020-12-12 22:27:20 +0800

    update 10-MyCustom/MyTheme(AppData)

--- AppData.tmpl/Projects/WIN10XPE/10-MyCustom/MyTheme/last.bat
+++ AppData.tmpl/Projects/WIN10XPE/10-MyCustom/MyTheme/last.bat
@@ -1,6 +1,6 @@
-if "x%opt[mytheme.theme_id]%"=="x" goto :EOF
+if "x%opt[mycustom.theme_id]%"=="x" goto :EOF
 
-echo [Example]Theme Id: %opt[mytheme.theme_id]%
+echo [Example]Theme Id: %opt[mycustom.theme_id]%
 
-echo [Example]7z.exe x "%~dp0Web%opt[mytheme.theme_id]%.7z" -y -aos -o"%X_WIN%\"
-echo [Example]copy /y startup%opt[mytheme.theme_id]%.wav "%X_WIN%\Media\startup.wav"
+echo [Example]7z.exe x "%~dp0Web%opt[mycustom.theme_id]%.7z" -y -aos -o"%X_WIN%\"
+echo [Example]copy /y startup%opt[mycustom.theme_id]%.wav "%X_WIN%\Media\startup.wav"

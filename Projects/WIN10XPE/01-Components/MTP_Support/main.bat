fed869ac4cff8605b00d9a42c642fa8e574549bb
slore <slorelee@gmail.com>
2020-12-15 03:09:23 +0800

    update MTP_Support/main.bat

--- Projects/WIN10XPE/01-Components/MTP_Support/main.bat
+++ Projects/WIN10XPE/01-Components/MTP_Support/main.bat
@@ -22,11 +22,16 @@ rem call AddFiles WUDFPlatform.dll MUI
 
 rem ==========update filesystem==========
 
+reg query HKLM\Tmp_DRIVERS\DriverDatabase\DriverInfFiles\wpdmtp.inf 1>nul 2>nul
+if %errorlevel% EQU 0 set _regdriver_done=1
+
 set AddFiles_Mode=merge
 
 call SharedPatch WINUSB
 
-call AddDrivers "wpd*.inf,wudf*.inf"
+call AddDrivers "wpdcomp.inf,wpdfs.inf,wpdmtp.inf,wpdmtphw.inf,wudfusbcciddriver.inf"
+rem Digital Still Camera
+rem call AddDrivers ts_wpdmtp.inf
 
 call AddFiles %0 :end_files
 goto :end_files
@@ -39,16 +44,41 @@ Microsoft-Windows-Portable-Devices-merged-Package~*.cat
 
 @windows\system32\
 drivers\UMDF\
+drivers\WpdUpFltr.sys
+drivers\WUDFPf.sys
+drivers\WUDFRd.sys
 wpd*,WUDF*,PortableDevice*
 
 ; copy file(s) (PC -> Phone)
 vaultcli.dll
+efswrt.dll
 
 :end_files
 
 call DoAddFiles
 
 rem ==========update registry==========
+
+if "x%_regdriver_done%"=="x" (
+    rem add DriverDatabase\DeviceIds
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\{50dd5230-ba8a-11d1-bf5d-0000f805f530}
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\{eec5ad98-8080-425f-922a-dabf3de3f69a}
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\COMP\WPD
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\wpdbusenum\fs
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\BthMtpEnum\{9518e5ca-f6af-464b-9907-a97433641968}]
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\UMB\urn:microsoft-com:device:mtp:1
+
+    rem call RegCopyDeviceIds USB wpdmtp.inf
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\USB\MS_COMP_MTP
+
+    call RegCopyDeviceIds USB wpdmtphw.inf
+    call RegCopyDeviceIds USB wudfusbcciddriver.inf
+
+    rem Digital Still Camera
+    rem call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\TS_USB
+)
+set _regdriver_done=
+
 rem WPD class(Portable Devices)
 call RegCopy SYSTEM\ControlSet001\Control\Class\{EEC5AD98-8080-425F-922A-DABF3DE3F69A}
 

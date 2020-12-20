bae838f0e633be2c7aac4095dd420c92d27b2055
slore <slorelee@gmail.com>
2020-12-20 22:19:08 +0800

    update RNDIS/main.bat

--- Projects/WIN10XPE/01-Components/RNDIS/main.bat
+++ Projects/WIN10XPE/01-Components/RNDIS/main.bat
@@ -4,6 +4,10 @@ set AddFiles_Mode=merge
 
 call SharedPatch WINUSB
 
+set _regdriver_done=
+reg query HKLM\Tmp_DRIVERS\DriverDatabase\DriverInfFiles\netrndis.inf 1>nul 2>nul
+if %errorlevel% EQU 0 set _regdriver_done=1
+
 call AddFiles %0 :end_files
 goto :end_files
 @\Windows\System32\drivers\
@@ -19,3 +23,14 @@ rem some devices need
 call AddDrivers wceisvista.inf
 
 call DoAddFiles
+
+
+if "x%_regdriver_done%"=="x" (
+    rem add DriverDatabase\DeviceIds
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\{4d36e972-e325-11ce-bfc1-08002be10318}
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\ms_rndisusb
+    call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\ms_rndisusb6
+    call RegCopy "HKLM\Drivers\DriverDatabase\DeviceIds\USB\Class_EF&SubClass_04&Prot_01"
+    call RegCopy "HKLM\Drivers\DriverDatabase\DeviceIds\USB\MS_COMP_RNDIS&MS_SUBCOMP_5162001"
+)
+set _regdriver_done=

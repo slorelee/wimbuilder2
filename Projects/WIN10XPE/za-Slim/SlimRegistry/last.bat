a02043d78f0d0b962b0ce8191c0ea68a27d9add7
slore <slorelee@gmail.com>
2021-01-01 15:36:25 +0800

    update SlimRegistry(SOFTWARE)

--- Projects/WIN10XPE/za-Slim/SlimRegistry/last.bat
+++ Projects/WIN10XPE/za-Slim/SlimRegistry/last.bat
@@ -44,21 +44,21 @@ goto :EOF
 
 :CLEANUP_SOFTWARE
 if not "x%opt[slim.hive_software]%"=="xtrue" goto :EOF
+set opt[registry.software.compress]=true
+reg delete "HKLM\Tmp_SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing" /f
 
 set "WINXSHELL=%V%\WinXShell\X_PF\WinXShell\WinXShell_%WB_ARCH%.exe"
 if not exist "%WINXSHELL%" goto :EOF
-
-set opt[registry.software.compress]=true
-if exist RemoveInvaildItems_Reg.txt del /q RemoveInvaildItems_Reg.txt
-if exist WinSxS_Manifests.txt del /q WinSxS_Manifests.txt
-dir /b "%X_WIN%\WinSxS\Manifests\*.manifest" > WinSxS_Manifests.txt
+if exist _RemoveInvaildItems_Reg.txt del /q _RemoveInvaildItems_Reg.txt
+if exist _WinSxS_Manifests.txt del /q _WinSxS_Manifests.txt
+dir /b "%X_WIN%\WinSxS\Manifests\*.manifest" > _WinSxS_Manifests.txt
 
 if exist RemoveInvaildRegItems.lua (
     "%WINXSHELL%" -console -script "%dp0%RemoveInvaildRegItems.lua"
 ) else (
     "%WINXSHELL%" -console -script "%dp0%RemoveInvaildRegItems.bin"
 )
-reg import RemoveInvaildItems_Reg.txt
+reg import _RemoveInvaildItems_Reg.txt
 
 rem hide "IE History and Feeds Shell Data Source for Windows Search" folder on Desktop
 reg add "HKLM\Tmp_SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v {11016101-E366-4D22-BC06-4ADA335C892B} /t REG_DWORD /d 1 /f

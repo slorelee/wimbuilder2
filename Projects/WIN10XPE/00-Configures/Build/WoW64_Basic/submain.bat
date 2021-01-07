323ab54dcb1217c94ac572e84884d5f278bd53cd
slore <slorelee@gmail.com>
2021-01-07 23:52:29 +0800

    update WoW64_Basic/submain.bat for windows 21h2 updated

--- Projects/WIN10XPE/00-Configures/Build/WoW64_Basic/submain.bat
+++ Projects/WIN10XPE/00-Configures/Build/WoW64_Basic/submain.bat
@@ -44,6 +44,9 @@ goto :end_files
 ; Not required with build 16299 \Windows\System32\SetWoW64.exe(loadWoW64.exe)
 ntdll.dll
 wow64.dll,wow64cpu.dll,wow64win.dll,wowreg32.exe
++ver >= 21286
+wow64base.dll
++ver*
 
 @windows\SysWOW64\
 C_*.NLS,KBD*.dll

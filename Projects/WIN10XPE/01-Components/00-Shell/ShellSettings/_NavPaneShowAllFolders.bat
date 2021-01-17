bda784eb6b9d4cb42b3571e391a403679245efb8
slore <slorelee@gmail.com>
2021-01-17 19:51:39 +0800

    update ShellSettings

--- Projects/WIN10XPE/01-Components/00-Shell/ShellSettings/_NavPaneShowAllFolders.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/ShellSettings/_NavPaneShowAllFolders.bat
@@ -1,1 +1,1 @@
-reg add HKLM\tmp_DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v NavPaneShowAllFolders /t REG_DWORD /d 1 /f
+rem reg add HKLM\tmp_DEFAULT\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v NavPaneShowAllFolders /t REG_DWORD /d 1 /f

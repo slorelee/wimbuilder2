e29a22558f7ea31fdd3b7e9830fa0b801ca3729c
slore <slorelee@gmail.com>
2021-02-02 10:28:22 +0900

    fix issue that a empty Temp folder on X drive

--- Projects/WIN10XPE/02-Apps/HotSwap/main.bat
+++ Projects/WIN10XPE/02-Apps/HotSwap/main.bat
@@ -1,6 +1,6 @@
-call V2X HotSwap -Extract "HotSwap_*.ZIP" "%X%\Temp\HotSwap\"
-copy /y "%X%\Temp\HotSwap\%_V3264%bit\HotSwap^!.EXE" "%X_SYS%\"
-rd /s /q "%X%\Temp\HotSwap\"
+call V2X HotSwap -Extract "HotSwap_*.ZIP" "%X_WIN%\Temp\HotSwap\"
+copy /y "%X_WIN%\Temp\HotSwap\%_V3264%bit\HotSwap^!.EXE" "%X_SYS%\"
+rd /s /q "%X_WIN%\Temp\HotSwap\"
 
 call AddFiles \Windows\system32\systray.exe
 reg add "HKLM\Tmp_Default\Software\HotSwap^!" /v DFlags /t REG_DWORD /d 0x10000003 /f

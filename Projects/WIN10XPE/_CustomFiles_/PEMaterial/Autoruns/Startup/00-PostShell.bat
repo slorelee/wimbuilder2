5e0a135b3fc239880e523957f6e2f2e35a5fcb39
slore <slorelee@gmail.com>
2021-03-24 23:56:25 +0800

    Show localized name for "Desktop" folder

--- Projects/WIN10XPE/_CustomFiles_/PEMaterial/Autoruns/Startup/00-PostShell.bat
+++ Projects/WIN10XPE/_CustomFiles_/PEMaterial/Autoruns/Startup/00-PostShell.bat
@@ -7,6 +7,9 @@ del /a /q "X:\Users\Default\Desktop\desktop.ini"
 del /a /q "X:\Users\Public\Desktop\desktop.ini"
 del /a /q "%USERPROFILE%\Desktop\desktop.ini"
 
+rem -- Show localized name for "Desktop" folder
+start WinXShell.exe -code "reg_write([[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}]], '', app:call('resstr', '#{@shell32.dll,21769}'))"
+
 rem -- clear Pinned icons by other User (Switch User)
 del /a /q "%APPDATA%\Microsoft\Internet Explorer\Quick Launch\User Pinned\StartMenu\*.lnk"
 del /a /q "%APPDATA%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*.lnk"

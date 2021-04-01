82ffadcb562b527f169fffa6c0ece0f00ef1013a
slore <slorelee@gmail.com>
2021-04-02 00:29:48 +0800

    support leave_main.bat and leave_last.bat action (WB_LEAVE_PATCH)

--- bin/ApplyProjectPatches.bat
+++ bin/ApplyProjectPatches.bat
@@ -32,6 +32,21 @@ call :apply_patch "%_pt_path%\%_WB_WALK_CMD%"
 goto :EOF
 
 :apply_patch
+if "x%WB_LEAVE_PATCH%"=="x" goto :end_leave_patch
+set "_patch_path=%~dp1"
+call set "_not_sub_path=%%_patch_path:%WB_LEAVE_PATCH%=%%"
+if "x%_not_sub_path%"=="x%_patch_path%" (
+    if exist "%WB_LEAVE_PATCH%leave_%~nx1" (
+        pushd "%WB_LEAVE_PATCH%"
+        echo \033[96mApplying Patch:%WB_LEAVE_PATCH%leave_%~nx1 | cmdcolor.exe
+        call "leave_%~nx1"
+        popd
+    )
+    set WB_LEAVE_PATCH=
+)
+set _not_sub_path=
+set _patch_path=
+:end_leave_patch
 if not exist "%~1" goto :EOF
 pushd "%~dp1"
 echo \033[96mApplying Patch:%~dpnx1 | cmdcolor.exe

0f17cf4e82f70fd389fd75804280e7b991beba11
slore <slorelee@gmail.com>
2021-04-03 16:03:34 +0800

    update APP_Dism++.bat

--- Projects/WIN10XPE/02-PEMaterial/01-Maintenance/APP_Dism++.bat
+++ Projects/WIN10XPE/02-PEMaterial/01-Maintenance/APP_Dism++.bat
@@ -1,3 +1,14 @@
 call V2X "%APP_CACHE%" -extract "Dism*.zip" "%X_PF%\Dism++\"
+
+if "%WB_PE_LANG%"=="zh-CN" goto :LINK_zh-CN
+
+:LINK_en-US
+call LinkToDesktop Dism++.lnk "#pProgramFiles#p\Dism++\Dism++%_Vx8664%.exe"
+call LinkToStartMenu "ImageTools\Dism++.lnk" "#pProgramFiles#p\Dism++\Dism++%_Vx8664%.exe"
+goto :EOF
+
+
+:LINK_zh-CN
 call LinkToDesktop Dism++镜像工具.lnk "#pProgramFiles#p\Dism++\Dism++%_Vx8664%.exe"
 call LinkToStartMenu "镜像制作\Dism++.lnk" "#pProgramFiles#p\Dism++\Dism++%_Vx8664%.exe"
+goto :EOF

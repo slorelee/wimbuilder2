f7cdcc4edb63603e1ba104546907166267589789
slore <slorelee@gmail.com>
2021-05-23 00:14:16 +0800

    add CredDialog

--- Projects/WIN10XPE/01-Components/CredDialog/main.bat
+++ Projects/WIN10XPE/01-Components/CredDialog/main.bat
@@ -1,5 +1,24 @@
-rem Patch Windows.UI.CredDialogController.dll to use the Credentials Window than Credentials  Console
-rem M.i.n.i.N.T => N.i.n.i.N.T
-binmay.exe -u "%X_SYS%\Windows.UI.CredDialogController.dll" -s u:MiniNT -r u:NiniNT
-fc /b "%X_SYS%\Windows.UI.CredDialogController.dll.org" "%X_SYS%\Windows.UI.CredDialogController.dll"
-del /q "%X_SYS%\Windows.UI.CredDialogController.dll.org"
+call AddFiles %0 :end_files
+goto :end_files
+
+\Windows\SystemResources\Windows.UI.Cred
+
+@\Windows\System32\
+;in winre.wim 
+;ConsoleLogon.dll,CredentialUIBroker.exe
+;CredProv2faHelper.dll,CredProvDataModel.dll,credprovhost.dll,credprovs.dll
+networkexplorer.dll
+;ntlanman.dll,ntshrui.dll,wincredui.dll
+;Windows.Internal.UI.Logon.ProxyStub.dll
+Windows.UI.Cred.dll
+;Windows.UI.CredDialogController.dll
+
+FontGlyphAnimator.dll
+threadpoolwinrt.dll
+Windows.UI.Xaml.Controls.dll
+Windows.UI.Xaml.dll
+Windows.UI.Xaml.Resources.Common.dll
+Windows.UI.Xaml.Resources%VER_XAMLRES%.dll
+Windows.UI.XamlHost.dll
+
+:end_files

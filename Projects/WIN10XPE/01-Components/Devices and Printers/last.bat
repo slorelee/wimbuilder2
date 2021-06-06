54877300ddcaa69b47c710d56b451c20d6f1f7be
slore <slorelee@gmail.com>
2021-06-06 12:35:17 +0800

    update 01-Components/Devices and Printers

--- Projects/WIN10XPE/01-Components/Devices and Printers/last.bat
+++ Projects/WIN10XPE/01-Components/Devices and Printers/last.bat
@@ -2,3 +2,11 @@ rem DsmSvc Patch Feature
 binmay.exe -u "%X_SYS%\DeviceSetupManager.dll" -s u:SystemSetupInProgress -r u:DisableDeviceSetupMgr
 fc /b "%X_SYS%\DeviceSetupManager.dll.org" "%X_SYS%\DeviceSetupManager.dll"
 del /f /q "%X_SYS%\DeviceSetupManager.dll.org"
+
+if "%WB_PE_ARCH%"=="x64" (
+    binmay.exe -u "%X_SYS%\DeviceSetupManager.dll" -s "81 FE 20 03 00 00" -r "81 FE 02 00 00 00"
+) else (
+    binmay.exe -u "%X_SYS%\DeviceSetupManager.dll" -s "45 F8 3D 20 03 00 00" -r "45 F8 3D 02 00 00 00"
+)
+fc /b "%X_SYS%\DeviceSetupManager.dll.org" "%X_SYS%\DeviceSetupManager.dll"
+del /f /q "%X_SYS%\DeviceSetupManager.dll.org"

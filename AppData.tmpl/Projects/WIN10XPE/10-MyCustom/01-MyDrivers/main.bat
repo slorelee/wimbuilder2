e870b5958807758ee8428d770a9c3c88d02d9a8b
slore <slorelee@gmail.com>
2021-08-16 22:40:25 +0900

    update AppData.tmpl/Projects/WIN10XPE/10-MyCustom/01-MyDrivers/main.bat

--- AppData.tmpl/Projects/WIN10XPE/10-MyCustom/01-MyDrivers/main.bat
+++ AppData.tmpl/Projects/WIN10XPE/10-MyCustom/01-MyDrivers/main.bat
@@ -1,3 +1,12 @@
+set _has_drive=
+for /f %%i in  ('dir /b /ad Drivers_%WB_PE_ARCH%') do set _has_drive=%%i
+if "x%_has_drive%"=="x" (
+    echo [INFO] %~dp0Drivers_%WB_PE_ARCH% is empty.
+    goto :EOF
+)
+set _has_drive=
+
+
 set _RELOAD_HIVE=0
 reg query HKLM\Tmp_SOFTWARE /ve 1>nul 2>nul
 if %errorlevel% EQU 0 (

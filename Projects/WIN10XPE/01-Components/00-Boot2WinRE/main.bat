059983de0d5e171744810725e1780c2619d5372d
slore <slorelee@gmail.com>
2021-10-10 11:22:16 +0800

    update 01-Components/00-Boot2WinRE

--- Projects/WIN10XPE/01-Components/00-Boot2WinRE/main.bat
+++ Projects/WIN10XPE/01-Components/00-Boot2WinRE/main.bat
@@ -10,12 +10,18 @@ mlang.dat,mlang.dll,mshta.exe,mshtml*.*,msimtf.dll,msoert2.dll,msrating.dll,oled
 pnppropmig.dll,ReserveManager.dll,storagewmi.dll,storagewmi_passthru.dll,unbcl.dll
 wdsutil.dll,webplatstorageserver.dll,wfdprov.dll,WiFiConfigSP.dll,WiFiDisplay.dll
 
+;WLAN
+wlan*.dll
+
 ;vwifi
 drivers\vwifibus.sys
 drivers\vwifimp.sys
+
+;iscsi
+iscsi*.dll,iscsicli.exe,iscsicpl.exe
 
 :end_files
 
 call RegCopy "HKLM\System\ControlSet001\Control\Network\{4d36e974-e325-11ce-bfc1-08002be10318}\{5CBF81BF-5055-47CD-9055-A76B2B4E3698}"
 call RegCopy "HKLM\System\ControlSet001\Control\WMI\Autologger\WiFiSession"
-call RegCopyEx Services "vwifibus,vwififlt,wcncsvc,wdiwifi,WFPLWFS,NativeWifiP,MSiSCSI"
+call RegCopyEx Services "vwifibus,vwififlt,wcncsvc,wdiwifi,WFPLWFS,WlanSvc,NativeWifiP,MSiSCSI"

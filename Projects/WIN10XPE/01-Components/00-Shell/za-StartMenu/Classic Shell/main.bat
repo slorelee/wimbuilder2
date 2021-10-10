9c8fcfd38a1d8fca50abf9db1f7e217c69b5d9af
slore <slorelee@gmail.com>
2021-10-10 12:11:54 +0800

    update 01-Components/00-Shell/za-StartMenu/Classic Shell/main.bat

--- Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/Classic Shell/main.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/Classic Shell/main.bat
@@ -1,2 +1,9 @@
 if not "x%opt[shell.startmenu]%"=="xClassicShell" goto :EOF
 call V2X "Classic Shell"
+
+rem Account Pictures
+set "_AccPic_Path=%X%\ProgramData\Microsoft\User Account Pictures"
+if not exist "%_AccPic_Path%\" mkdir "%_AccPic_Path%"
+copy /y "%WB_ROOT%\%APPDATA_DIR%\_CustomFiles_\AccountPictures\user-200.png" "%_AccPic_Path%\"
+set _AccPic_Path=
+

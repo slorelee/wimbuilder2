47baa8065af0f48ae5fb52fd02d973870a8fcf2f
slore <slorelee@gmail.com>
2021-12-24 00:28:01 +0800

    update 02-Apps/Explorer++/main.bat

--- Projects/WIN10XPE/02-Apps/Explorer++/main.bat
+++ Projects/WIN10XPE/02-Apps/Explorer++/main.bat
@@ -1,130 +1,72 @@
-set APP_File=explorer++_1.3.5_x%_V8664%.zip
+set app_ver=1.3.5
 set APP_NAME=Explorer++
-call App pull https://explorerplusplus.com/software/%APP_FILE%
+call App pull https://explorerplusplus.com/software/explorer++_%app_ver%_x%_V8664%.zip
+call :BUILD
+call :GETLANGPACKS "%~nx0"
+call :SHORTCUTS
+
+set app_ver=
+goto :EOF
+
+:BUILD
 call V2X "%APP_CACHE%" -extract "%APP_FILE%" "%X_PF%\%APP_NAME%\"
 del /f /q /a "%X_PF%\%APP_NAME%\*.txt"
+goto :EOF
+
+:GETLANGPACKS
+if not "x%app_ver%"=="x1.3.5" goto :EOF
+for /f "tokens=2,3 delims=,=" %%i in ('%findcmd% "LangPacks[%WB_PE_LANG%" "%~1"') do (
+    set app_langpack=explorer++_%app_ver%_%%i.zip
+    set app_langid=%%j
+)
+if "x%app_langpack%"=="x" (
+    echo [INFO] No "%WB_PE_LANG%" translation available for Explorer++ %app_ver%. 
+    goto :EOF
+)
+call App pull https://explorerplusplus.com/software/translations/%app_langpack%
+call V2X "%APP_CACHE%" -extract "%app_langpack%" "%X_PF%\%APP_NAME%\"
+reg add "HKLM\Tmp_Default\Software\Explorer++\Settings" /v "Language" /t REG_DWORD /d %app_langid% /f
+set app_langpack=
+set app_langid=
+goto :EOF
+
+:SHORTCUTS
 call LinkToDesktop "%APP_NAME%.lnk" "#pProgramFiles#p\%APP_NAME%\%APP_NAME%.exe"
 call LinkToStartMenu "%APP_NAME%\%APP_NAME%.lnk" "#pProgramFiles#p\%APP_NAME%\%APP_NAME%.exe"
 call PinToTaskbar "#pProgramFiles#p\%APP_NAME%\%APP_NAME%.exe"
 call PinToStartmenu "#pProgramFiles#p\%APP_NAME%\%APP_NAME%.exe"
-
-rem ---- Language Selection. ----
-if "%WB_PE_LANG%"=="cs-CZ" (
-    set APP_LANG=explorer++_1.3.5_CS.zip
-    set LNG_VALUE=0x05
-    call :LANG
-)
-if "%WB_PE_LANG%"=="da-DK" (
-    set APP_LANG=explorer++_1.3.5_DA.zip
-    set LNG_VALUE=0x06
-    call :LANG
-)
-if "%WB_PE_LANG%"=="de-DE" (
-    set APP_LANG=explorer++_1.3.5_DE.zip
-    set LNG_VALUE=0x07
-    call :LANG
-)
-if "%WB_PE_LANG%"=="es-ES" (
-    set APP_LANG=explorer++_1.3.5_ES.zip
-    set LNG_VALUE=0x0a
-    call :LANG
-)
-if "%WB_PE_LANG%"=="fr-FR" (
-    set APP_LANG=explorer++_1.3.5_FR.zip
-    set LNG_VALUE=0x0c
-    call :LANG
-)
-if "%WB_PE_LANG%"=="hu-HU" (
-    set APP_LANG=explorer++_1.3.5_HU.zip
-    set LNG_VALUE=0x0e
-    call :LANG
-)
-if "%WB_PE_LANG%"=="it-IT" (
-    set APP_LANG=explorer++_1.3.5_IT.zip
-    set LNG_VALUE=0x10
-    call :LANG
-)
-if "%WB_PE_LANG%"=="ja-jp" (
-    set APP_LANG=explorer++_1.3.5_JA.zip
-    set LNG_VALUE=0x11
-    call :LANG
-)
-if "%WB_PE_LANG%"=="ko-KR" (
-    set APP_LANG=explorer++_1.3.5_KO.zip
-    set LNG_VALUE=0x12
-    call :LANG
-)
-if "%WB_PE_LANG%"=="nb-NO" (
-    set APP_LANG=explorer++_1.3.5_NO.zip
-    set LNG_VALUE=0x14
-    call :LANG
-)
-if "%WB_PE_LANG%"=="nl-NL" (
-    set APP_LANG=explorer++_1.3.5_NL.zip
-    set LNG_VALUE=0x13
-    call :LANG
-)
-if "%WB_PE_LANG%"=="pl-PL" (
-    set APP_LANG=explorer++_1.3.5_PL.zip
-    set LNG_VALUE=0x15
-    call :LANG
-)
-if "%WB_PE_LANG%"=="pt-PT" (
-    set APP_LANG=explorer++_1.3.5_PT.zip
-    set LNG_VALUE=0x16
-    call :LANG
-)
-if "%WB_PE_LANG%"=="ro-RO" (
-    set APP_LANG=explorer++_1.3.5_RO.zip
-    set LNG_VALUE=0x18
-    call :LANG
-)
-if "%WB_PE_LANG%"=="ru-RU" (
-    set APP_LANG=explorer++_1.3.5_RU.zip
-    set LNG_VALUE=0x19
-    call :LANG
-)
-if "%WB_PE_LANG%"=="sv-SE" (
-    set APP_LANG=explorer++_1.3.5_SV.zip
-    set LNG_VALUE=0x1d
-    call :LANG
-)
-if "%WB_PE_LANG%"=="tr-TR" (
-    set APP_LANG=explorer++_1.3.5_TR.zip
-    set LNG_VALUE=0x1f
-    call :LANG
-)
-if "%WB_PE_LANG%"=="uk-UA" (
-    set APP_LANG=explorer++_1.3.5_UK.zip
-    set LNG_VALUE=0x22
-    call :LANG
-)
-if "%WB_PE_LANG%"=="zh-CN" (
-    set APP_LANG=explorer++_1.3.5_ZH.zip
-    set LNG_VALUE=0x04
-    call :LANG
-)
-call :MANUAL
-
-:LANG
-call App pull https://explorerplusplus.com/software/translations/%APP_LANG%
-call V2X "%APP_CACHE%" -extract "%APP_LANG%" "%X_PF%\%APP_NAME%\"
-reg add "HKLM\Tmp_Default\Software\Explorer++\Settings" /v "Language" /t REG_DWORD /d "%LNG_VALUE%" /f
 goto :EOF
 
-:MANUAL
-rem If your language can not detect automaticaly,
-rem uncomment it below, if it exist.
-rem Catalan (ca-ES), avaiable in LangPacks only:
-rem call App pull https://explorerplusplus.com/software/translations/explorer++_1.3.5_CA.zip
-rem call V2X "%APP_CACHE%" -extract "explorer++_1.3.5_CA.zip" "%X_PF%\%APP_NAME%\"
-rem reg add "HKLM\Tmp_Default\Software\Explorer++\Settings" /v "Language" /t REG_DWORD /d 0x03 /f
-rem Farsi, in Microsoft docs i not found it:
-rem call App pull https://explorerplusplus.com/software/translations/explorer++_1.3.5_FA.zip
-rem call V2X "%APP_CACHE%" -extract "explorer++_1.3.5_FA.zip" "%X_PF%\%APP_NAME%\"
-rem reg add "HKLM\Tmp_Default\Software\Explorer++\Settings" /v "Language" /t REG_DWORD /d 0x29 /f
-rem Sinhala (si-LK), avaiable in LangPacks only:
-rem call App pull https://explorerplusplus.com/software/translations/explorer++_1.3.5_SI.zip
-rem call V2X "%APP_CACHE%" -extract "explorer++_1.3.5_SI.zip" "%X_PF%\%APP_NAME%\"
-rem reg add "HKLM\Tmp_Default\Software\Explorer++\Settings" /v "Language" /t REG_DWORD /d 0x5b /f
+:LANGPACKS_INFO
+rem 1.3.5
+rem https://explorerplusplus.com/translations
+LangPacks[ca-ES]=CA,0x03
+LangPacks[cs-CZ]=CS,0x05
+LangPacks[da-DK]=DA,0x06
+LangPacks[de-DE]=DE,0x07
+LangPacks[es-ES]=ES,0x0a
+LangPacks[fa-IR]=FA,0x29
+LangPacks[fr-FR]=FR,0x0c
+LangPacks[hu-HU]=HU,0x0e
+LangPacks[it-IT]=IT,0x10
+LangPacks[ja-jp]=JA,0x11
+LangPacks[ko-KR]=KO,0x12
+LangPacks[nl-NL]=NL,0x13
+LangPacks[nb-NO]=NO,0x14
+LangPacks[pl-PL]=PL,0x15
+LangPacks[pt-PT]=PT,0x16
+LangPacks[ro-RO]=RO,0x18
+LangPacks[ru-RU]=RU,0x19
+LangPacks[si-LK]=SI,0x5b
+LangPacks[sv-SE]=SV,0x1d
+LangPacks[tr-TR]=TR,0x1f
+LangPacks[uk-UA]=UK,0x22
+LangPacks[zh-CN]=ZH,0x04
+rem 1.4.0
+rem https://github.com/derceg/explorerplusplus/tree/master/Translations
+LangPacks140[fi-FI]=FI,0x40b
+LangPacks140[he-IL]=HE,0x40d
+LangPacks140[vi-VN]=VI,0x42a
+LangPacks140[zh-CN]=ZH_CN,0x804
+LangPacks140[zh-TW]=ZH_TW,0x404
 goto :EOF

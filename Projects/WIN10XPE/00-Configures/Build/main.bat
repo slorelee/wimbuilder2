44a922d1eacec402fc6c6116856d7ad139628d71
slore <slorelee@gmail.com>
2021-12-23 21:17:37 +0800

    update registry with  a reg file than "reg add" commands

--- Projects/WIN10XPE/00-Configures/Build/main.bat
+++ Projects/WIN10XPE/00-Configures/Build/main.bat
@@ -11,9 +11,13 @@ call RegCopy HKLM\Software\Classes\AppID
 call ACLRegKey HKLM\Software\Classes\AppID
 
 rem set "RunAs"="Interactive User" -* "RunAs"=""
+echo REGEDIT4 > "%WB_TMP_PATH%\RunAsUpdateTmp.reg"
+echo. >> "%WB_TMP_PATH%\RunAsUpdateTmp.reg"
 for /F %%i IN ('Reg Query HKLM\Tmp_Software\Classes\AppID /s /f "Interactive User" ^|%findcmd% Tmp_Software') do (
-    Reg Add "%%i" /v RunAs /d "" /F >nul 2>nul
-)
+    echo [%%i]
+    echo "RunAs"=""
+) >> "%WB_TMP_PATH%\RunAsUpdateTmp.reg"
+reg import "%WB_TMP_PATH%\RunAsUpdateTmp.reg"
 
 call RegCopy HKLM\Software\Classes\CLSID
 call RegCopy HKLM\Software\Classes\Interface

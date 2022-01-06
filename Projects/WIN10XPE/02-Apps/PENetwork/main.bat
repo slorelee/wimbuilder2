22e06279ad27b1afe15bb6c01819afc4cfb51135
Ruslan <96018301+rupelodn@users.noreply.github.com>
2022-01-06 17:28:46 +0300

    WimBuilder2\Projects\WIN10XPE\02-Apps\PENetwork\main.bat
    Cleaning unnecessary files:
    The unnecessary languages and two license files are cleaned.
    After startup WinPE, no one will read these licenses, and the language will not be changed either.
    I think there are no violations, because the archive with the original version of the program remains unchanged.

--- Projects/WIN10XPE/02-Apps/PENetwork/main.bat
+++ Projects/WIN10XPE/02-Apps/PENetwork/main.bat
@@ -2,3 +2,10 @@ call V2X PENetwork -extract "PENetwork%_V_x64%.7z" "%X_PF%\PENetwork\"
 reg import PENetwork_Settings.reg
 
 call LinkToDesktop PENetwork.lnk "#pProgramFiles#p\PENetwork\PENetwork.exe"
+
+del /f /q /a "%X_PF%\PENetwork\*.txt"
+if exist "%X_PF%\PENetwork\PENetwork_%WB_PE_LANG%.lng" (
+  ren "%X_PF%\PENetwork\PENetwork_%WB_PE_LANG%.lng" "PENetwork_%WB_PE_LANG%.bak"
+  del /f /q /a "%X_PF%\PENetwork\*.lng"
+  ren "%X_PF%\PENetwork\PENetwork_%WB_PE_LANG%.bak" "PENetwork_%WB_PE_LANG%.lng"
+)

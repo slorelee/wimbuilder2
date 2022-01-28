8f9030e03549a177c7ff2781c64a15bf2e3e1378
Ruslan <96018301+rupelodn@users.noreply.github.com>
2022-01-28 11:17:54 +0300

    WimBuilder2\Projects\WIN10XPE\02-Apps\Defraggler (final)
    Hi slore, I think this is the final version.
    Everything that can be optimized, reg files are completely removed, new macros are taken into account.
    Thank you for your help!

--- Projects/WIN10XPE/02-Apps/Defraggler/main.bat
+++ Projects/WIN10XPE/02-Apps/Defraggler/main.bat
@@ -1,84 +1,67 @@
 set APP_NAME=Defraggler
 call App pull https://download.ccleaner.com/dfsetup222.exe
-call V2X "%APP_CACHE%" -extract "dfsetup222.exe" "%X_WIN%\%APP_NAME%\"
+call V2X "%APP_CACHE%" -extract "%APP_FILE%" "%X_WIN%\%APP_NAME%\"
 md "%X_PF%\%APP_NAME%"
-call :copy-%_Vx8664%
-reg import "Defraggler%_Vx8664%.reg"
-call :COPYLANGFILE "%~nx0"
-if exist "Defraggler.%WB_PE_LANG%.reg" reg import "Defraggler.%WB_PE_LANG%.reg"
-if "%WB_PE_ARCH%"=="x64" (
-    call LinkToStartMenu "System Tools\%APP_NAME%.lnk" "#pProgramFiles#p\%APP_NAME%\%APP_NAME%64.exe"
-) else (
-    call LinkToStartMenu "System Tools\%APP_NAME%.lnk" "#pProgramFiles#p\%APP_NAME%\%APP_NAME%.exe"
-)
+copy /y "%X_WIN%\%APP_NAME%\%APP_NAME%%_V64%.exe" "%X_PF%\%APP_NAME%\"
+copy /y "%X_WIN%\%APP_NAME%\DefragglerShell%_V64%.dll.new" "%X_PF%\%APP_NAME%\DefragglerShell%_V64%.dll"
+copy /y "%X_WIN%\%APP_NAME%\df%_V64%.exe" "%X_PF%\%APP_NAME%\"
+call :MainReg
+if not %WB_PE_LANG%=="en-US" call :COPYLANGFILE
+call LinkToStartMenu "System Tools\%APP_NAME%.lnk" "#pProgramFiles#p\%APP_NAME%\%APP_NAME%%_V64%.exe"
 rd /s /q "%X_WIN%\%APP_NAME%"
 goto :EOF
 
-:copy-x86
-copy /y "%X_WIN%\%APP_NAME%\%APP_NAME%.exe" "%X_PF%\%APP_NAME%\"
-copy /y "%X_WIN%\%APP_NAME%\DefragglerShell.dll.new" "%X_PF%\%APP_NAME%\DefragglerShell.dll"
-copy /y "%X_WIN%\%APP_NAME%\df.exe" "%X_PF%\%APP_NAME%\"
-goto :EOF
-
-:copy-x64
-copy /y "%X_WIN%\%APP_NAME%\%APP_NAME%64.exe" "%X_PF%\%APP_NAME%\"
-copy /y "%X_WIN%\%APP_NAME%\DefragglerShell64.dll.new" "%X_PF%\%APP_NAME%\DefragglerShell64.dll"
-copy /y "%X_WIN%\%APP_NAME%\df64.exe" "%X_PF%\%APP_NAME%\"
+:MainReg
+reg add "HKLM\Tmp_Software\Classes\*\shellex\ContextMenuHandlers\DefragglerShellExtension" /ve /d "{4380C993-0C43-4E02-9A7A-0D40B6EA7590}" /f
+reg add "HKLM\Tmp_Software\Classes\CLSID\{4380C993-0C43-4E02-9A7A-0D40B6EA7590}" /ve /d "DefragglerShellExtension Class" /f
+reg add "HKLM\Tmp_Software\Classes\CLSID\{4380C993-0C43-4E02-9A7A-0D40B6EA7590}\InprocServer32" /ve /d "X:\Program Files\Defraggler\DefragglerShell%_V64%.dll" /f
+reg add "HKLM\Tmp_Software\Classes\CLSID\{4380C993-0C43-4E02-9A7A-0D40B6EA7590}\InprocServer32" /v "ThreadingModel" /d "Apartment" /f
+reg add "HKLM\Tmp_Software\Classes\folder\ShellEx\ContextMenuHandlers\DefragglerShellExtension" /ve /d "{4380C993-0C43-4E02-9A7A-0D40B6EA7590}" /f
+reg add "HKLM\Tmp_Software\Microsoft\Windows\CurrentVersion\App Paths\defraggler%_V64%.exe" /ve /d "X:\Program Files\Defraggler\Defraggler%_V64%.exe" /f
+reg add "HKLM\Tmp_Software\Microsoft\Windows\CurrentVersion\App Paths\defraggler%_V64%.exe" /v "Path" /d "X:\Program Files\Defraggler" /f
+reg add "HKLM\Tmp_Software\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\DefragPath" /ve /d "X:\Program Files\Defraggler\Defraggler%_V64%.exe" /f
+reg add "HKLM\Tmp_Software\Microsoft\Windows\CurrentVersion\Extensions\Approved\{4380C993-0C43-4E02-9A7A-0D40B6EA7590}" /ve /d "DefragglerShellExtension" /f
+reg add "HKLM\Tmp_Software\Piriform\Defraggler" /v "PreviousDefragPath" /d "%%systemroot%%\system32\dfrgui.exe" /f
+reg add "HKLM\Tmp_Software\Piriform\Defraggler" /v "ReplaceWindowsDefrag" /t REG_DWORD /d 0x1 /f
+reg add "HKLM\Tmp_Software\Piriform\Defraggler" /v "UpdateCheck" /d "0" /f
+reg add "HKLM\Tmp_Software\Piriform\Defraggler" /ve /d "X:\Program Files\Defraggler" /f
+reg add "HKLM\Tmp_Default\Software\Classes\Software\Piriform\Defraggler" /v "Executable" /d "Defraggler%_V64%.exe" /f
+reg add "HKLM\Tmp_Default\Software\Classes\Software\Piriform\Defraggler" /v "InstallPath" /d "X:\Program Files\Defraggler" /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "AnalyzeContextString" /d "&Check Fragmentation" /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "DefragContextString" /d "&Defragment" /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "DisableShellExtension" /t REG_DWORD /d 0x0 /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "Executable" /d "Defraggler%_V64%.exe" /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "InstallPath" /d "X:\Program Files\Defraggler" /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "Language" /d "1033" /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "UpdateBackground" /t REG_DWORD /d 0x0 /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "UpdateCheck" /t REG_DWORD /d 0x0 /f
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "WasRunManualSilentUpdate" /t REG_DWORD /d 0x0 /f
 goto :EOF
 
 :COPYLANGFILE
-if %WB_PE_LANG%=="en-US" goto :EOF
-for /f "tokens=2,3 delims=,=" %%i in ('%findcmd% "LangFile[%WB_PE_LANG%" "%~1"') do (
-    set app_langfile=%%i
+call GetLocaleId %WB_PE_LANG%
+set app_langid=%GetLocaleId_Ret%
+if "%WB_PE_LANG%"=="ku-ARAB-IQ" (
+    set app_langid=9999
+) else if "%WB_PE_LANG%"=="es-ES" (
+   set app_langid=1034
 )
-if "x%app_langfile%"=="x" (
-    echo [INFO] No "%WB_PE_LANG%" translation available for "%APP_NAME%". 
-    reg import "Defraggler.en-US.reg"
+set app_langfile=lang-%app_langid%.dll
+if not exist "%X_WIN%\%APP_NAME%\Lang\%app_langfile%" (
+    echo [INFO] No "%WB_PE_LANG%" translation available for "%APP_NAME%".
     goto :EOF
 )
+echo [INFO] Found %app_langfile% resource for "%APP_NAME%".
 md "%X_PF%\%APP_NAME%\Lang"
 copy /y "%X_WIN%\%APP_NAME%\Lang\%app_langfile%" "%X_PF%\%APP_NAME%\Lang\"
+reg add "HKLM\Tmp_Default\Software\Piriform\Defraggler" /v "Language" /d "%app_langid%" /f
+if not "%APP_HOST_LANG%"=="%WB_PE_LANG%" (
+    copy /y DefragglerContextMenuUpdater.bat "%X_Startup%\"
+    call TextReplace "%X_Startup%\DefragglerContextMenuUpdater.bat" APP_LANGFILE %app_langfile% g
+) else (
+"%WINXSHELL%" -code "reg_write([[HKEY_LOCAL_MACHINE\Tmp_Default\Software\Piriform\Defraggler]], 'AnalyzeContextString', app:call('resstr', [[#{@%X_PF%\%APP_NAME%\Lang\%app_langfile%,505}]]))"
+"%WINXSHELL%" -code "reg_write([[HKEY_LOCAL_MACHINE\Tmp_Default\Software\Piriform\Defraggler]], 'DefragContextString', app:call('resstr', [[#{@%X_PF%\%APP_NAME%\Lang\%app_langfile%,504}]]))"
+)
 set app_langfile=
+set app_langid=
 goto :EOF
-
-:LangFile_INFO
-LangFile[ar-SA]=lang-1025.dll
-LangFile[be-BY]=lang-1059.dll
-LangFile[bg-BG]=lang-1026.dll
-LangFile[ca-ES]=lang-1027.dll
-LangFile[cs-CZ]=lang-1029.dll
-LangFile[da-DK]=lang-1030.dll
-LangFile[de-DE]=lang-1031.dll
-LangFile[el-GR]=lang-1032.dll
-LangFile[es-ES]=lang-1034.dll
-LangFile[et-EE]=lang-1061.dll
-LangFile[fa-IR]=lang-1065.dll
-LangFile[fi-FI]=lang-1035.dll
-LangFile[fr-FR]=lang-1036.dll
-LangFile[he-IL]=lang-1037.dll
-LangFile[hr-HR]=lang-1050.dll
-LangFile[hu-HU]=lang-1038.dll
-LangFile[hy-AM]=lang-1067.dll
-LangFile[id-ID]=lang-1057.dll
-LangFile[it-IT]=lang-1040.dll
-LangFile[ja-jp]=lang-1041.dll
-LangFile[ka-GE]=lang-1079.dll
-LangFile[lt-LT]=lang-1063.dll
-LangFile[lv-LV]=lang-1062.dll
-LangFile[mk-MK]=lang-1071.dll
-LangFile[nb-NO]=lang-1044.dll
-LangFile[nl-NL]=lang-1043.dll
-LangFile[pl-PL]=lang-1045.dll
-LangFile[pt-BR]=lang-1046.dll
-LangFile[pt-PT]=lang-2070.dll
-LangFile[ro-RO]=lang-1048.dll
-LangFile[ru-RU]=lang-1049.dll
-LangFile[sk-SK]=lang-1051.dll
-LangFile[sl-SI]=lang-1060.dll
-LangFile[sq-AL]=lang-1052.dll
-LangFile[sv-SE]=lang-1053.dll
-LangFile[tr-TR]=lang-1055.dll
-LangFile[uk-UA]=lang-1058.dll
-LangFile[vi-VN]=lang-1066.dll
-LangFile[zh-CN]=lang-2052.dll
-LangFile[zh-TW]=lang-1028.dll

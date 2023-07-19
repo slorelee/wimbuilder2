79670d4082d763b394bbd408659cd158a7e5d33d
slore <slorelee@gmail.com>
2023-07-19 20:19:57 +0800

    update for StartMenu items

--- Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/StartIsBack/last.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/StartIsBack/last.bat
@@ -1,3 +1,6 @@
 rem incompatible with StartIsBack(SYSTEM account)
 del /q "%X_SYS%\windows.immersiveshell.serviceprovider.dll"
 
+rem // hide useless contextmenu
+reg add "HKLM\Tmp_DEFAULT\Software\Policies\Microsoft\Windows\Explorer" /v HidePeopleBar /t REG_DWORD /d 1 /f 
+reg add "HKLM\Tmp_DEFAULT\Software\Policies\Microsoft\Windows\Explorer" /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f 

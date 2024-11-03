d7c92f6360341e22a5cc80df3b50326e7b54c3cd
slore <slorelee@gmail.com>
2024-11-03 23:30:49 +0800

    fix issue that the shell will crash when clicking the Show Desktop button

--- Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/StartIsBack/last.bat
+++ Projects/WIN10XPE/01-Components/00-Shell/za-StartMenu/StartIsBack/last.bat
@@ -1,8 +1,13 @@
+if "x%opt[shell.startmenu]%"=="xStartAllBack" (
+  goto :update_registry
+)
+
 rem incompatible with StartIsBack(SYSTEM account)
 if not exist "%X_SYS%\seclogon.dll" (
   del /q "%X_SYS%\windows.immersiveshell.serviceprovider.dll"
 )
 
+:update_registry
 rem // hide useless contextmenu
 reg add "HKLM\Tmp_DEFAULT\Software\Policies\Microsoft\Windows\Explorer" /v HidePeopleBar /t REG_DWORD /d 1 /f 
 reg add "HKLM\Tmp_DEFAULT\Software\Policies\Microsoft\Windows\Explorer" /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f 

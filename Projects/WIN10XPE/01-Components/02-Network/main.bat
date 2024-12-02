b97218e5e6d8dc6c9dc19cdc97cff7c2bf519278
slore <slorelee@gmail.com>
2024-12-02 23:54:23 +0800

    update 02-Network/main.bat

--- Projects/WIN10XPE/01-Components/02-Network/main.bat
+++ Projects/WIN10XPE/01-Components/02-Network/main.bat
@@ -9,6 +9,11 @@ if "x%opt[component.PPPoE]%"=="xtrue" (
 if not exist "%X_SYS%\wlanapi.dll" (
   call :ApplySubPatch "..\00-Boot2WinRE" _WinPE-WiFi-Package.bat
 )
+
+if "x%opt[build.registry.software]%"=="xfull" (
+  reg add "HKLM\Tmp_SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\Capabilities\wlanLocationBypass" /v RequireWindowsCert /t REG_DWORD /d 0 /f
+)
+
 call _networktrayicon.bat
 call full_functional.bat
 call netdrivers.bat

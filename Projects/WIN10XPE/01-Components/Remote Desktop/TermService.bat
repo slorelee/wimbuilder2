718aa5953e287bc67aaf5cd68c900c2d2f14ffef
slore <slorelee@gmail.com>
2025-01-05 23:43:21 +0800

    update 01-Components/Remote Desktop/TermService.bat for Windows 11 (22000 and later)

--- Projects/WIN10XPE/01-Components/Remote Desktop/TermService.bat
+++ Projects/WIN10XPE/01-Components/Remote Desktop/TermService.bat
@@ -44,13 +44,31 @@ CertProp.dll
 CertPropSvc.dll
 wwancfg.dll
 
+;v1903(optional), v21H2(required)
++ver >= 22000
+\Windows\WinSxS\*rdpidd*
+drivers\IndirectKmd.sys
+WppRecorderUM.dll
+
+;for DWM
+DispBroker.dll
+DispBroker.Desktop.dll
++ver*
+
 :end_files
 
 call AddDrivers "rdpbus.inf,rdlsbuscbs.inf,termkbd.inf,termmou.inf"
 call AddDrivers "monitor.inf"
 call AddDrivers "tsprint.inf,tsusbhub.inf,tsusbhubfilter.inf,tsgenericusbdriver.inf"
-
+if %VER[3]% GEQ 22000 (
+  call AddDrivers "rdpidd.inf"
+)
 call DoAddFiles
+
+if %VER[3]% GEQ 22000 (
+  rem rdpidd needs WUDF
+  call ApplyPatch "..\MTP_Support"
+)
 
 rem ==========update registry==========
 
@@ -58,9 +76,13 @@ if not "x%opt[build.registry.software]%"=="xfull" (
   call RegCopy "HKLM\Software\Microsoft\Terminal Server"
 )
 
-rem Disable WDDM Driver (posted by smine)
-rem http://bbs.wuyou.net/forum.php?mod=redirect&goto=findpost&ptid=411399&pid=4304128
-reg add "HKLM\Tmp_Software\Policies\Microsoft\Windows NT\Terminal Services" /v fEnableWddmDriver /t REG_DWORD /d 0 /f
+if %VER[3]% GEQ 22000 (
+  call RegCopyEx Services "IndirectKmd"
+) else (
+  rem Disable WDDM Driver for win10 v1903 and later ^(posted by smine^)
+  rem http://bbs.wuyou.net/forum.php?mod=redirect&goto=findpost&ptid=411399&pid=4304128
+  reg add "HKLM\Tmp_Software\Policies\Microsoft\Windows NT\Terminal Services" /v fEnableWddmDriver /t REG_DWORD /d 0 /f
+)
 
 call RegCopy "HKLM\System\ControlSet001\Control\Terminal Server"
 call ACLRegKey "HKLM\System\ControlSet001\Control\Terminal Server\RCM"

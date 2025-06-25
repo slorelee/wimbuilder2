8741397daf63b8aa5765095581b42cc497c3d85d
slore <slorelee@gmail.com>
2025-06-25 08:04:01 +0800

    move drvinst updater to 01-Drivers

--- Projects/WIN10XPE/01-Components/main.bat
+++
@@ -1,5 +0,0 @@
-if "x%opt[patch.drvinst]%"=="xtrue" (
-  call ApplyPatch ".\Patch_drvinst"
-)
-
-goto :EOF
---
+++ Projects/WIN10XPE/01-Drivers/main.bat
@@ -0,0 +1,3 @@
+if "x%opt[patch.drvinst]%"=="xtrue" (
+  call ApplyPatch "..\01-Components\Patch_drvinst"
+)

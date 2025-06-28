6f3d2a2935035df4b3fa549c208ddda3202868d5
slore <slorelee@gmail.com>
2025-06-29 03:30:22 +0800

    update drvinst patcher for 24h2

--- Projects/WIN10XPE/01-Components/Patch_drvinst/main.bat
+++ Projects/WIN10XPE/01-Components/Patch_drvinst/main.bat
@@ -41,7 +41,8 @@ if %VER[3]% GTR 17700 set VER_NAME=win10.rs5later
 if %VER[3]% GTR 18908 set VER_NAME=win10.18908later
 if %VER[3]% GEQ 19041 set VER_NAME=win10.20h1later
 if %VER[3]% GEQ 22621 set VER_NAME=win11.22h2later
-if %VER[3]% GEQ 26100 set VER_NAME=win11.24h2later
+if %VER[3]% EQU 26100 if %VER[4]% GEQ 3775 set VER_NAME=win11.24h2april2025later
+if %VER[3]% GTR 26100 set VER_NAME=win11.25h2later
 call :Drvinst_%VER_NAME%_%WB_PE_ARCH%
 if "%PATCH_MODE%"=="local" pause
 goto :EOF
@@ -90,7 +91,8 @@ goto :EOF
 call :FULL_PATCH 8BF8_85C0_7423 33C0_8BF8_EB23
 goto :EOF
 
-:Drvinst_win11.24h2later_x64
+:Drvinst_win11.24h2april2025later_x64
+:Drvinst_win11.25h2later_x64
 call :FULL_PATCH 8BF8_85C0_7422 33C0_8BF8_EB22
 goto :EOF
 

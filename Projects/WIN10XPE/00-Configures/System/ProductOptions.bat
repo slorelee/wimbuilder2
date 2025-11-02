e865042cad68fdb79363e84bd77b6688ad171587
slore <slorelee@gmail.com>
2025-11-03 01:26:20 +0800

    update ProductOptions.bat

--- Projects/WIN10XPE/00-Configures/System/ProductOptions.bat
+++ Projects/WIN10XPE/00-Configures/System/ProductOptions.bat
@@ -1,6 +1,18 @@
 rem some features are dependence on this.
 rem like: SnippingTool, TermService, etc.
 
+rem use install.wim's ProductOptions (show version watermark on desktop)
+call RegCopy HKLM\SYSTEM\ControlSet001\Control\ProductOptions
+reg delete "HKLM\Tmp_SYSTEM\ControlSet001\Control\ProductOptions" /v OSProductContentId /f
+reg delete "HKLM\Tmp_SYSTEM\ControlSet001\Control\ProductOptions" /v OSProductPfn /f
+reg delete "HKLM\Tmp_SYSTEM\ControlSet001\Control\ProductOptions" /v SubscriptionPfnList /f
+
+"%WINXSHELL%" -code "App:Call('exitcode', Reg.PolicySet == nil or 1)"
+if not ERRORLEVEL 1 goto :USE_USER_PRODUCTOPTIONS
+"%WINXSHELL%" -script "%~dp0ProductOptionsUpdater.lua"
+goto :EOF
+
+:USE_USER_PRODUCTOPTIONS
 if exist "%_USER_CUSTOMFILES_%\ProductOptions.txt" (
   reg import "%_USER_CUSTOMFILES_%\ProductOptions.txt"
   goto :EOF
@@ -11,8 +23,3 @@ if exist "%WB_PROJECT_PATH%\_CustomFiles_\ProductOptions.txt" (
   goto :EOF
 )
 
-rem use install.wim's ProductOptions (show version watermark on desktop)
-call RegCopy HKLM\SYSTEM\ControlSet001\Control\ProductOptions
-reg del HKLM\Tmp_SYSTEM\ControlSet001\Control\ProductOptions /v OSProductContentId /f
-reg del HKLM\Tmp_SYSTEM\ControlSet001\Control\ProductOptions /v OSProductPfn /f
-reg del HKLM\Tmp_SYSTEM\ControlSet001\Control\ProductOptions /v SubscriptionPfnList /f

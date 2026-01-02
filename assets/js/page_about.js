a7a33898c61d4049d38cf44dfb2213d09c7e07b0
slore <slorelee@gmail.com>
2026-01-02 19:59:01 +0800

    improve update button

--- assets/js/page_about.js
+++ assets/js/page_about.js
@@ -1,11 +1,13 @@
-function update_me() {
-  if ($ui_settings['update_source'] == 'github' && !fso.FileExists(eformat('%Factory%\\tmp\\remote.md5'))) {
+function update_me(repo) {
+  if (repo == null && $ui_settings['update_source'] == 'github' &&
+    !fso.FileExists(eformat('%APP_ROOT%\\assets\\update.js')) &&
+    !fso.FileExists(eformat('%Factory%\\tmp\\remote.md5'))) {
     var msg = i18n_t('Will update from %s, you can change the update source in [Settings] page.');
     msg = msg.replace('%s', $("#ui_update option:selected").text());
     alert(msg);
   }
 
-  app_env_init();
+  app_env_init(repo);
   Run('_Updater.cmd')
 }
 

49c9c6a05e5a7bd3afdbc4725685da92adbb3c34
slore <slorelee@gmail.com>
2026-03-09 00:35:01 +0800

    update 00-Configures/System/last.bat

--- Projects/WIN10XPE/00-Configures/System/last.bat
+++ Projects/WIN10XPE/00-Configures/System/last.bat
@@ -7,5 +7,8 @@ reg add "HKLM\Tmp_Software\Microsoft\Windows NT\CurrentVersion\ProfileList\S-1-5
 rem NumLock on/off
 if not "x%opt[system.numlock]%"=="xfalse" (
     reg add "HKLM\Tmp_Default\Control Panel\Keyboard" /v InitialKeyboardIndicators /d 2 /f
+) else (
+    reg add "HKLM\Tmp_Default\Control Panel\Keyboard" /v InitialKeyboardIndicators /d 0 /f
+    if exist "%X_SYS%\Pecmd.ini" call TextReplace "%X_SYS%\Pecmd.ini" "NUMK 1" "NUMK 0" g
 )
 goto :EOF
